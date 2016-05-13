b93e1f5ff71bf30634812285624ff88f653d7a76
Steve Davids <sdavids@gmail.com>
2016-05-12 20:52:05 -0400

    Added gigasecond module

---
+++ exercises/gigasecond/src/example/kotlin/Gigasecond.kt
@@ -0,0 +1,9 @@
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+data class Gigasecond(val initialDateTime: LocalDateTime) {
+
+    constructor(initialDate: LocalDate): this(initialDate.atTime(0, 0))
+
+    val date = initialDateTime.plusSeconds(1000000000)
+}
