f8272b3e46242ddddd3aad7ea1cb3204749af070
Steve Davids <sdavids@gmail.com>
2016-05-14 17:53:34 -0400

    Added scrabble-score module

---
+++ exercises/scrabble-score/src/example/kotlin/Scrabble.kt
@@ -0,0 +1,7 @@
+object Scrabble {
+    private val scoreToLetters = mapOf(1 to "AEIOULNRST", 2 to "DG", 3 to "BCMP", 4 to "FHVWY", 5 to "K", 8 to "JX", 10 to "QZ").mapValues { it.value.toCharArray() }
+    private val letterToScore = scoreToLetters.flatMap { entry -> entry.value.map { char -> Pair(char, entry.key) } }.toMap()
+
+    fun scoreLetter(c: Char) = letterToScore[c.toUpperCase()] ?: 0
+    fun scoreWord(word: String) = word.fold(0, { score, char -> score + scoreLetter(char) })
+}
---
+++ exercises/scrabble-score/src/test/kotlin/ScrabbleScoreTest.kt
@@ -0,0 +1,28 @@
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import kotlin.test.assertEquals
+
+@RunWith(Parameterized::class)
+class ScrabbleScoreTest(val input: String, val expectedOutput: Int) {
+
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters(name = "{index}: scoreWord({0})={1}")
+        fun data() = listOf(
+                arrayOf("", 0),
+                arrayOf(" \t\n", 0),
+                arrayOf("a", 1),
+                arrayOf("f", 4),
+                arrayOf("street", 6),
+                arrayOf("quirky", 22),
+                arrayOf("OXYPHENBUTAZONE", 41),
+                arrayOf("alacrity", 13)
+        )
+    }
+
+    @Test
+    fun test() {
+        assertEquals(expectedOutput, Scrabble.scoreWord(input));
+    }
+}
