352475a349bf572a4c03bd103881f64bfc841f03
Steve Davids <sdavids@gmail.com>
2016-05-14 20:57:47 -0400

    Added linked-list module

---
+++ exercises/linked-list/src/example/kotlin/Deque.kt
@@ -0,0 +1,45 @@
+class Deque<T> {
+    private var head: Element<T>? = null
+
+    fun push(value: T) {
+        if(head == null) {
+            head = Element(value)
+            head?.prev = head
+            head?.next = head
+        } else {
+            val oldTail = head?.prev
+            val tail = Element(value, prev = oldTail, next = head)
+            oldTail?.next = tail
+            head?.prev = tail
+        }
+
+    }
+
+    fun pop(): T? {
+        head = head?.prev
+        return shift()
+    }
+
+    fun unshift(value: T) {
+        push(value);
+        head = head?.prev;
+    }
+
+    fun shift(): T? {
+        val value = head?.value;
+        val newHead = head?.next;
+        val newTail = head?.prev;
+
+        if (newHead == head) {
+            head = null;
+        } else {
+            newHead?.prev = newTail;
+            newTail?.next = newHead;
+            head = newHead;
+        }
+
+        return value;
+    }
+
+    private data class Element<T>(val value: T, var prev: Element<T>? = null, var next: Element<T>? = null)
+}
---
+++ exercises/linked-list/src/test/kotlin/DequeTest.kt
@@ -0,0 +1,60 @@
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class DequeTest {
+    private lateinit var subject: Deque<Int>;
+
+    @Before
+    fun setUp() {
+        subject = Deque();
+    }
+
+    @Test
+    fun pushPop() {
+        subject.push(10);
+        subject.push(20);
+        assertEquals(20, subject.pop())
+        assertEquals(10, subject.pop())
+    }
+
+    @Test
+    fun pushShift() {
+        subject.push(10);
+        subject.push(20);
+        assertEquals(10, subject.shift())
+        assertEquals(20, subject.shift())
+    }
+
+    @Test
+    fun unshiftShift() {
+        subject.unshift(10);
+        subject.unshift(20);
+        assertEquals(20, subject.shift())
+        assertEquals(10, subject.shift())
+    }
+
+    @Test
+    fun unshiftPop() {
+        subject.unshift(10);
+        subject.unshift(20);
+        assertEquals(10, subject.pop())
+        assertEquals(20, subject.pop())
+    }
+
+    @Test
+    fun example() {
+        subject.push(10);
+        subject.push(20);
+        assertEquals(20, subject.pop())
+
+        subject.push(30);
+        assertEquals(10, subject.shift())
+
+        subject.unshift(40);
+        subject.push(50);
+        assertEquals(40, subject.shift())
+        assertEquals(50, subject.pop())
+        assertEquals(30, subject.shift())
+    }
+}
