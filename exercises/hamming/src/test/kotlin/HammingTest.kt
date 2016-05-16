6a17434064430ac79a044bd3a65315c7b90fe218
Steve Davids <sdavids@gmail.com>
2016-05-16 18:36:51 -0400

    Removed test method name prefix

--- exercises/hamming/src/test/kotlin/HammingTest.kt
+++ exercises/hamming/src/test/kotlin/HammingTest.kt
@@ -4,47 +4,47 @@ import kotlin.test.assertEquals
 class HammingTest {
     
     @Test
-    fun testNoDifferenceBetweenIdenticalStrands() {
+    fun noDifferenceBetweenIdenticalStrands() {
         assertEquals(0, Hamming.compute("A", "A"))
     }
 
     @Test
-    fun testCompleteHammingDistanceOfForSingleNucleotideStrand() {
+    fun completeHammingDistanceOfForSingleNucleotideStrand() {
         assertEquals(1, Hamming.compute("A", "G"))
     }
 
     @Test
-    fun testCompleteHammingDistanceForSmallStrand() {
+    fun completeHammingDistanceForSmallStrand() {
         assertEquals(2, Hamming.compute("AG", "CT"))
     }
 
     @Test
-    fun testSmallHammingDistance() {
+    fun smallHammingDistance() {
         assertEquals(1, Hamming.compute("AT", "CT"))
     }
 
     @Test
-    fun testSmallHammingDistanceInLongerStrand() {
+    fun smallHammingDistanceInLongerStrand() {
         assertEquals(1, Hamming.compute("GGACG", "GGTCG"))
     }
 
     @Test(expected = IllegalArgumentException::class)
-    fun testValidatesFirstStrandNotLonger() {
+    fun validatesFirstStrandNotLonger() {
         Hamming.compute("AAAG", "AAA");
     }
 
     @Test(expected = IllegalArgumentException::class)
-    fun testValidatesOtherStrandNotLonger() {
+    fun validatesOtherStrandNotLonger() {
         Hamming.compute("AAA", "AAAG");
     }
 
     @Test
-    fun testLargeHammingDistance() {
+    fun largeHammingDistance() {
         assertEquals(4, Hamming.compute("GATACA", "GCATAA"))
     }
 
     @Test
-    fun testHammingDistanceInVeryLongStrand() {
+    fun hammingDistanceInVeryLongStrand() {
         assertEquals(9, Hamming.compute("GGACGGATTCTG", "AGGACGGATTCT"))
     }
 
