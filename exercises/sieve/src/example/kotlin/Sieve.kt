c9247a17d8cd6deb8d1aaf6c9ccc0fe7eb9893c5
Steve Davids <sdavids@gmail.com>
2016-05-16 22:13:18 -0400

    Added sieve module

---
+++ exercises/sieve/src/example/kotlin/Sieve.kt
@@ -0,0 +1,13 @@
+object Sieve {
+    fun primesUpTo(upperBound: Int): List<Int> {
+        val primes = mutableListOf<Int>()
+        var candidates = (2..upperBound).toList()
+        while (candidates.size > 0) {
+            val prime = candidates[0]
+            primes += prime
+            candidates = candidates.filterIndexed { index, value -> index != 0 && value % prime != 0 }
+        }
+
+        return primes
+    }
+}
