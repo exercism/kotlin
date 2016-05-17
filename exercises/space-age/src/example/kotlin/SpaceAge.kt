bc77b31e7bb255f78a493c008cadaecfd646cc1d
Steve Davids <sdavids@gmail.com>
2016-05-16 22:46:12 -0400

    Added space-age module

---
+++ exercises/space-age/src/example/kotlin/SpaceAge.kt
@@ -0,0 +1,29 @@
+import java.math.BigDecimal
+
+class SpaceAge(val seconds: Long) {
+
+    companion object {
+        val EARTH_ORBITAL_PERIOD_IN_SECONDS = 31557600.0
+        val PRECISION = 2
+
+        private enum class Planet(val relativeOrbitalPeriod: Double) {
+            EARTH(1.0), MERCURY(0.2408467), VENUS(0.61519726), MARS(1.8808158),
+            JUPITER(11.862615), SATURN(29.447498), URANUS(84.016846), NEPTUNE(164.79132)
+        }
+    }
+
+    fun onEarth() = calculateAge(Planet.EARTH)
+    fun onMercury() = calculateAge(Planet.MERCURY)
+    fun onVenus() = calculateAge(Planet.VENUS)
+    fun onMars() = calculateAge(Planet.MARS)
+    fun onJupiter() = calculateAge(Planet.JUPITER)
+    fun onSaturn() = calculateAge(Planet.SATURN)
+    fun onUranus() = calculateAge(Planet.URANUS)
+    fun onNeptune() = calculateAge(Planet.NEPTUNE)
+
+    private fun calculateAge(planet: Planet): Double {
+        val age: Double = seconds / (EARTH_ORBITAL_PERIOD_IN_SECONDS * planet.relativeOrbitalPeriod)
+
+        return BigDecimal(age).setScale(PRECISION, BigDecimal.ROUND_HALF_UP).toDouble()
+    }
+}
---
+++ exercises/space-age/src/test/kotlin/SpaceAgeTest.kt
@@ -0,0 +1,75 @@
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class SpaceAgeTest {
+
+    @Test
+    fun ageInSeconds() {
+        val age = SpaceAge(1000000)
+
+        assertEquals(1000000, age.seconds)
+    }
+
+    @Test
+    fun ageOnEarth() {
+        val age = SpaceAge(1000000000)
+
+        assertEquals(31.69, age.onEarth())
+    }
+
+    @Test
+    fun ageOnMercury() {
+        val age = SpaceAge(2134835688)
+
+        assertEquals(67.65, age.onEarth())
+        assertEquals(280.88, age.onMercury())
+    }
+
+    @Test
+    fun ageOnVenus() {
+        val age = SpaceAge(189839836)
+
+        assertEquals(6.02, age.onEarth())
+        assertEquals(9.78, age.onVenus())
+    }
+
+    @Test
+    fun ageOnMars() {
+        val age = SpaceAge(2329871239L)
+
+        assertEquals(73.83, age.onEarth())
+        assertEquals(39.25, age.onMars())
+    }
+
+    @Test
+    fun ageOnJupiter() {
+        val age = SpaceAge(901876382)
+
+        assertEquals(28.58, age.onEarth())
+        assertEquals(2.41, age.onJupiter())
+    }
+
+    @Test
+    fun ageOnSaturn() {
+        val age = SpaceAge(3000000000L)
+
+        assertEquals(95.06, age.onEarth())
+        assertEquals(3.23, age.onSaturn())
+    }
+
+    @Test
+    fun ageOnUranus() {
+        val age = SpaceAge(3210123456L)
+
+        assertEquals(101.72, age.onEarth())
+        assertEquals(1.21, age.onUranus())
+    }
+
+    @Test
+    fun ageOnNeptune() {
+        val age = SpaceAge(8210123456L)
+
+        assertEquals(260.16, age.onEarth())
+        assertEquals(1.58, age.onNeptune())
+    }
+}
