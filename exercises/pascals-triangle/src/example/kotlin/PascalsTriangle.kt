b7cd38756e10051892844610316d25bfb33352ca
Steve Davids <sdavids@gmail.com>
2016-05-18 21:14:12 -0400

    Provided better naming in example

--- exercises/pascals-triangle/src/example/kotlin/PascalsTriangle.kt
+++ exercises/pascals-triangle/src/example/kotlin/PascalsTriangle.kt
@@ -4,13 +4,13 @@ object PascalsTriangle {
         require(rows >= 0, { "Rows can't be negative!" })
 
         if (rows == 0) return emptyList()
-        return (1..rows).map { row -> triRow(row) }
+        return (1..rows).map { buildTriangleRow(it) }
     }
 
-    private fun triRow(row: Int): List<Int> {
+    private fun buildTriangleRow(row: Int): List<Int> {
         var m = 1
-        return listOf(1) + (1..row - 1).map { j ->
-            m = m * (row - j) / j
+        return listOf(1) + (1..row - 1).map { col ->
+            m = m * (row - col) / col
             return@map m
         }
     }
