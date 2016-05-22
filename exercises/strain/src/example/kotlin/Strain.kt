80dc0f8f4edd6f9e1376222006b2d8390c245e92
Steve Davids <sdavids@gmail.com>
2016-05-22 13:02:52 -0400

    Followed instructions by not using `filter`

--- exercises/strain/src/example/kotlin/Strain.kt
+++ exercises/strain/src/example/kotlin/Strain.kt
@@ -1,4 +1,10 @@
 object Strain {
-    fun <T> keep(collection: List<T>, predicate: (T) -> Boolean) = collection.filter(predicate)
-    fun <T> discard(collection: List<T>, predicate: (T) -> Boolean) = collection.filterNot(predicate)
+    fun <T> keep(collection: List<T>, predicate: (T) -> Boolean): List<T> {
+        val filteredCollection = mutableListOf<T>()
+        collection.forEach { if (predicate(it)) filteredCollection.add(it) }
+
+        return filteredCollection
+    }
+
+    fun <T> discard(collection: List<T>, predicate: (T) -> Boolean) = keep(collection, { value -> !predicate(value) })
 }
