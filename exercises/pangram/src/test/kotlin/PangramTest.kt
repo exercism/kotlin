73566c47254bbd83bc25b87ef96bf97dbeb0c70f
Steve Davids <sdavids@gmail.com>
2016-05-28 13:43:02 -0400

    Added additional pangram tests per issue #6.

--- exercises/pangram/src/test/kotlin/PangramTest.kt
+++ exercises/pangram/src/test/kotlin/PangramTest.kt
@@ -10,13 +10,33 @@ class PangramTest {
     }
 
     @Test
-    fun testLowercasePangram() {
+    fun lowercasePangram() {
         assertTrue(Pangrams.isPangram("the quick brown fox jumps over the lazy dog"))
     }
 
     @Test
     fun missingCharacterX() {
         assertFalse(Pangrams.isPangram("a quick movement of the enemy will jeopardize five gunboats"))
+    }
+
+    @Test
+    fun anotherMissingCharacterX() {
+        assertFalse(Pangrams.isPangram("the quick brown fish jumps over the lazy dog"))
+    }
+
+    @Test
+    fun withUnderscores() {
+        assertTrue(Pangrams.isPangram("the_quick_brown_fox_jumps_over_the_lazy_dog"))
+    }
+
+    @Test
+    fun withNumbers() {
+        assertTrue(Pangrams.isPangram("the 1 quick brown fox jumps over the 2 lazy dogs"))
+    }
+
+    @Test
+    fun missingLettersReplacedByNumbers() {
+        assertFalse(Pangrams.isPangram("7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog"))
     }
 
     @Test
