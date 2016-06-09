542576de6f810d95cb333dbee2e50afbd84496ce
gandrianakis <geoand@gmail.com>
2016-06-09 11:38:09 +0300

    Added an implementation of the bracket-push exercise

---
+++ exercises/bracket-push/src/test/kotlin/BracketPushTest.kt
@@ -0,0 +1,33 @@
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import kotlin.test.assertEquals
+
+@RunWith(Parameterized::class)
+class BracketPushTest(val input: String, val expectedOutput: Boolean) {
+
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters(name = "{index}: bracket({0})={1}")
+        fun data() = listOf(
+                arrayOf("[]", true),
+                arrayOf("", true),
+                arrayOf("[[", false),
+                arrayOf("}{", false),
+                arrayOf("{ }", true),
+                arrayOf("{[]}", true),
+                arrayOf("{}[]", true),
+                arrayOf("([{}({}[])])", true),
+                arrayOf("([{])", false),
+                arrayOf("[({]})", false),
+                arrayOf("(((185 + 223.85) * 15) - 543)/2", true),
+                arrayOf("\\\\left(\\\\begin{array}{cc} \\\\frac{1}{3} & x\\\\\\\\ \\\\mathrm{e}^{x} &... x^2 \\\\end{array}\\\\right)", true)
+        )
+    }
+
+    @Test
+    fun test() {
+        assertEquals(expectedOutput, BracketPush.isValid(input))
+    }
+}
