4d5f361e5189226661ef3bfef00d595ca66e1f1b
gandrianakis <geoand@gmail.com>
2016-06-09 12:15:49 +0300

    Added an implementation of the largest-series-product exercise

---
+++ exercises/largest-series-product/src/example/kotlin/Series.kt
@@ -0,0 +1,45 @@
+import java.lang.Math.max
+
+class Series(val digits: String) {
+
+    init {
+        require(digits.all { it.isDigit() })
+    }
+
+    fun calculate(span: Int): Long {
+        require(span >= 0)
+        require(digits.length >= span)
+
+        if(digits.isEmpty()) {
+            return 1
+        }
+
+
+        return calculateRec(digits.map { it.intValue() }, span, 0)
+    }
+
+    private tailrec fun calculateRec(digits: List<Int>, span: Int, maxSoFar: Long): Long {
+        val currentProduct = digits.subList(0, span).prod()
+        val newMax = max(currentProduct, maxSoFar)
+        if(digits.size == span) { //since we won't be checking 'remainders' of the list, the end condition is simply when the size is the same as the span
+            return newMax
+        }
+
+        return calculateRec(digits.tail(), span, newMax)
+    }
+
+}
+
+fun Char.intValue() : Int  {
+    if(!this.isDigit()) {
+        throw IllegalArgumentException("Char ${this} is not a legal DEC character")
+    }
+
+    return this.intDiff('0')
+}
+
+fun Char.intDiff(other: Char) = this.toInt() - other.toInt()
+
+fun List<Int>.prod() : Long = this.fold(1L) {productSoFar, item -> item*productSoFar}
+
+fun <T> List<T>.tail() = this.subList(1, this.size)
