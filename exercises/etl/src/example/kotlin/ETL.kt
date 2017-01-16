840ef223d19858dc4055fda50244592eb2407b29
John S. Ryan <jtigger@infosysengr.com>
2017-01-16 07:04:22 -0800

    Use data from description in tests.
    - per @sdavids13's suggestion: adjust collections to contain `Char`s
      instead of `String`s.

--- exercises/etl/src/example/kotlin/ETL.kt
+++ exercises/etl/src/example/kotlin/ETL.kt
@@ -1,5 +1,5 @@
 object ETL {
-    fun transform(old: Map<Int, Collection<String>>): Map<String, Int> {
+    fun transform(old: Map<Int, Collection<Char>>): Map<Char, Int> {
         return old.flatMap { mapEntry -> mapEntry.value.map { word -> Pair(word.toLowerCase(), mapEntry.key) } }.toMap()
     }
 }
