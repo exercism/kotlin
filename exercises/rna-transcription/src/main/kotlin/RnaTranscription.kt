7e09e4a0d4535976a028684b1621ef17d47e689b
Steve Davids <sdavids@gmail.com>
2017-01-17 20:30:14 -0500

    Moved snippet about top-level functions to the implementation location

--- exercises/rna-transcription/src/main/kotlin/RnaTranscription.kt
+++ exercises/rna-transcription/src/main/kotlin/RnaTranscription.kt
@@ -1,1 +1,9 @@
+/*
+  In Kotlin functions can be declared at top level in a file, meaning
+  you do not need to create a class to hold a function, like languages
+  such as Java, C# or Scala.
+
+  http://kotlinlang.org/docs/reference/functions.html#function-scope
+ */
+
 fun transcribeToRna(dna: String): String = ""
