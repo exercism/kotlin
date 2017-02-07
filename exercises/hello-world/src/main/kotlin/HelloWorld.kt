dfa43c89758ef8623cc6fdf3a6f587d301832118
John S. Ryan <jtigger@infosysengr.com>
2017-02-07 07:30:02 -0800

    Rework hello-world as a directed introduction to the track.
    - step-by-step tutorial gently introducing the practitioner to the language, the TDD process and Exercism.
    - the tests now flow so that each test draws shape in the solution.
    - moved `hello()` to the top-level, illustrating Kotlin's functions-as-first-class-citizens feature.

--- exercises/hello-world/src/main/kotlin/HelloWorld.kt
+++ exercises/hello-world/src/main/kotlin/HelloWorld.kt
@@ -1,31 +1,3 @@
-/**
- * Simple HelloWorld singleton class as defined by the `Kotlin object keyword`.
- * See: https://kotlinlang.org/docs/reference/object-declarations.html#object-declarations
- *
- * As an alternative one could create a class such as:
- * ```
- * class HelloWorld(name: String? = "Default Value") {
- *     fun hello(): String {
- *
- *     }
- * }
- * ```
- * Resulting in a call such as: `HelloWorld("Bob").hello()`
- * See: https://kotlinlang.org/docs/reference/classes.html#constructors
- *
- * In Kotlin we make objects defined as nullable via the trailing `?`, if you try
- * to assign a null value to any value that isn't nullable a compilation error is thrown.
- * Kotlin makes sure you are accessing nullable values safely and provides null safe calls
- * and the use of the elvis operator. See: https://kotlinlang.org/docs/reference/null-safety.html
- *
- * You may provide default values on methods, so if an argument is omitted the default is used.
- * See: https://kotlinlang.org/docs/reference/functions.html#default-arguments
- *
- * Kotlin provides String interpolation to make String formatting simple.
- * See: https://kotlinlang.org/docs/reference/idioms.html#string-interpolation
- */
-object HelloWorld {
-    fun hello(name: String? = "Default Argument"): String {
-
-    }
+fun hello(name: String = ""): String {
+   return ""
 }
