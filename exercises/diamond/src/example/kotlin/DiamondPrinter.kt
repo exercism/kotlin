f7d2abee1fcf0747c67333c74c1da65fda0cfd98
Stuart Kent <skent.dev@gmail.com>
2017-05-29 12:27:00 -0400

    diamond: add to track

---
+++ exercises/diamond/src/example/kotlin/DiamondPrinter.kt
@@ -0,0 +1,35 @@
+import java.util.*
+
+class DiamondPrinter {
+
+    companion object {
+        private val A_INT = 'A'.toInt()
+
+        private fun blank(length: Int): String {
+            return Collections.nCopies(length, " ").joinToString("")
+        }
+    }
+
+    fun printToList(chr: Char): List<String> {
+        val nRows = 2 * (chr.toInt() - A_INT) + 1
+
+        val result = mutableListOf<String>()
+
+        // Populate the top rows.
+        for (nRow in 0..(nRows + 1) / 2 - 1) {
+            val rowChr = (A_INT + nRow).toChar()
+
+            val leftHalfOfRow = blank((nRows - 1) / 2 - nRow) + rowChr + blank(nRow)
+            val rightHalfOfRow = leftHalfOfRow.reversed().drop(1)
+            val fullRow = "$leftHalfOfRow$rightHalfOfRow"
+
+            result.add(fullRow)
+        }
+
+        // Populate the bottom rows by 'reflecting' all rows above the middle row.
+        result.addAll(result.reversed().drop(1))
+
+        return result
+    }
+
+}
