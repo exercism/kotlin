511435e02003b49a0353de7f996ff515ad9cea2d
Stuart Kent <skent.dev@gmail.com>
2017-10-22 19:16:01 +0200

    flatten-array: update to v1.1.0
    https://github.com/exercism/problem-specifications/blob/d7a3c09b6758a8a5d59a2e140e6684031316280a/exercises/flatten-array/canonical-data.json

--- exercises/flatten-array/src/test/kotlin/FlattenerTest.kt
+++ exercises/flatten-array/src/test/kotlin/FlattenerTest.kt
@@ -4,24 +4,16 @@ import kotlin.test.assertEquals
 
 class FlattenerTest {
 
-
     @Test
-    fun flattensANestedList() {
-        val nestedList = listOf<Any>(emptyList<Any>())
-        assertEquals(emptyList<Any>(), Flattener.flatten(nestedList))
+    fun flatListIsUnchanged() {
+        val flatList = listOf(0, 1, 2)
+        assertEquals(listOf(0, 1, 2), Flattener.flatten(flatList))
     }
 
     @Ignore
     @Test
     fun flattens2LevelNestedList() {
-        val nestedList = listOf(1, listOf(2, 3, 4), 5)
-        assertEquals(listOf(1, 2, 3, 4, 5), Flattener.flatten(nestedList))
-    }
-
-    @Ignore
-    @Test
-    fun flattens3LevelNestedList() {
-        val nestedList = listOf(1, listOf(2, 3, 4), 5, listOf(6, listOf(7, 8)))
+        val nestedList = listOf(1, listOf(2, 3, 4, 5, 6, 7), 8)
         assertEquals(listOf(1, 2, 3, 4, 5, 6, 7, 8), Flattener.flatten(nestedList))
     }
 
@@ -42,15 +34,15 @@ class FlattenerTest {
     @Ignore
     @Test
     fun flattens6LevelNestedListWithNulls() {
-        val nestedList = listOf(1, listOf(2, null, listOf(listOf(3), null), listOf(4, listOf(listOf(5))), 6, 7, listOf(listOf(null))), 8, null)
-        assertEquals(listOf(1, 2, 3, 4, 5, 6, 7, 8), Flattener.flatten(nestedList))
+        val nestedList = listOf(0, 2, listOf(listOf(2, 3), 8, listOf(listOf(100)), null, listOf(listOf(null))), -2)
+        assertEquals(listOf(0, 2, 2, 3, 8, 100, -2), Flattener.flatten(nestedList))
     }
 
     @Ignore
     @Test
     fun allNullNestedListReturnsEmptyList() {
-        val nestedList = listOf(null, listOf(null, listOf(null), listOf(listOf(listOf(null)))), null)
+        val nestedList = listOf(null, listOf(listOf(listOf(null))), null, null, listOf(listOf(null, null), null), null)
         assertEquals(emptyList<Any>(), Flattener.flatten(nestedList))
     }
-}
 
+}
