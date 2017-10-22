d2a1f5c0ea80598fe3cbe75083f2f5e136076ae5
Stuart Kent <skent.dev@gmail.com>
2017-10-22 18:40:40 +0200

    space-age: update to v1.0.0
    https://github.com/exercism/problem-specifications/blob/7c63e40c8311d27416ef7582407226b9bea6993d/exercises/space-age/canonical-data.json

--- exercises/space-age/src/test/kotlin/SpaceAgeTest.kt
+++ exercises/space-age/src/test/kotlin/SpaceAgeTest.kt
@@ -4,15 +4,6 @@ import kotlin.test.assertEquals
 
 class SpaceAgeTest {
 
-
-    @Test
-    fun ageInSeconds() {
-        val age = SpaceAge(1000000)
-
-        assertEquals(1000000, age.seconds)
-    }
-
-    @Ignore
     @Test
     fun ageOnEarth() {
         val age = SpaceAge(1000000000)
@@ -25,7 +16,6 @@ class SpaceAgeTest {
     fun ageOnMercury() {
         val age = SpaceAge(2134835688)
 
-        assertEquals(67.65, age.onEarth())
         assertEquals(280.88, age.onMercury())
     }
 
@@ -34,7 +24,6 @@ class SpaceAgeTest {
     fun ageOnVenus() {
         val age = SpaceAge(189839836)
 
-        assertEquals(6.02, age.onEarth())
         assertEquals(9.78, age.onVenus())
     }
 
@@ -43,7 +32,6 @@ class SpaceAgeTest {
     fun ageOnMars() {
         val age = SpaceAge(2329871239L)
 
-        assertEquals(73.83, age.onEarth())
         assertEquals(39.25, age.onMars())
     }
 
@@ -52,7 +40,6 @@ class SpaceAgeTest {
     fun ageOnJupiter() {
         val age = SpaceAge(901876382)
 
-        assertEquals(28.58, age.onEarth())
         assertEquals(2.41, age.onJupiter())
     }
 
@@ -61,7 +48,6 @@ class SpaceAgeTest {
     fun ageOnSaturn() {
         val age = SpaceAge(3000000000L)
 
-        assertEquals(95.06, age.onEarth())
         assertEquals(3.23, age.onSaturn())
     }
 
@@ -70,7 +56,6 @@ class SpaceAgeTest {
     fun ageOnUranus() {
         val age = SpaceAge(3210123456L)
 
-        assertEquals(101.72, age.onEarth())
         assertEquals(1.21, age.onUranus())
     }
 
@@ -79,7 +64,7 @@ class SpaceAgeTest {
     fun ageOnNeptune() {
         val age = SpaceAge(8210123456L)
 
-        assertEquals(260.16, age.onEarth())
         assertEquals(1.58, age.onNeptune())
     }
+
 }
