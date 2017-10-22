ba8bcdb98a2ea867ced37ae0030a43158e4f8efd
Stuart Kent <skent.dev@gmail.com>
2017-10-22 18:50:24 +0200

    scrabble-score: update to v1.0.0
    https://github.com/exercism/problem-specifications/blob/11ed5039054a84b58bc7ccdda250d8a0121ccbed/exercises/scrabble-score/canonical-data.json

--- exercises/scrabble-score/.meta/src/reference/kotlin/ScrabbleScore.kt
+++ exercises/scrabble-score/.meta/src/reference/kotlin/ScrabbleScore.kt
@@ -1,4 +1,4 @@
-object Scrabble {
+object ScrabbleScore {
     private val scoreToLetters = mapOf(1 to "AEIOULNRST", 2 to "DG", 3 to "BCMP", 4 to "FHVWY", 5 to "K", 8 to "JX", 10 to "QZ").mapValues { it.value.toCharArray() }
     private val letterToScore = scoreToLetters.flatMap { entry -> entry.value.map { char -> Pair(char, entry.key) } }.toMap()
 
