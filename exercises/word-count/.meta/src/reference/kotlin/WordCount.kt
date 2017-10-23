8ce2994c8671f6e8079fe955c619649b3c1e0d1a
Stuart Kent <skent.dev@gmail.com>
2017-10-23 12:36:58 +0200

    word-count: update to v1.0.0
    https://github.com/exercism/problem-specifications/blob/cd26d49d1fb139e38b6c74a3ea3cdf3bf5a311df/exercises/word-count/canonical-data.json

--- exercises/word-count/.meta/src/reference/kotlin/WordCount.kt
+++ exercises/word-count/.meta/src/reference/kotlin/WordCount.kt
@@ -2,10 +2,14 @@ object WordCount {
 
     fun phrase(phrase: String): Map<String, Int> {
         val withoutPunctuation = phrase.toLowerCase().replace(Regex("[^\\w']"), " ").trim()
-        val words = withoutPunctuation.split(Regex("\\s+"))
-        val groupedWords = words.groupBy({ w -> w })
 
-        return groupedWords.mapValues({ it.value.size })
+        val words = withoutPunctuation.split(Regex("\\s+"))
+
+        val unquotedWords = words.map { word -> word.trim('\'') }
+
+        val groupedWords = unquotedWords.groupBy { w -> w }
+
+        return groupedWords.mapValues { it.value.size }
     }
 
 }
--- exercises/word-count/src/test/kotlin/WordCountTest.kt
+++ exercises/word-count/src/test/kotlin/WordCountTest.kt
@@ -4,7 +4,6 @@ import kotlin.test.assertEquals
 
 class WordCountTest {
 
-
     @Test
     fun countOneWord() {
         val expectedWordCount = mapOf("word" to 1)
@@ -22,7 +21,7 @@ class WordCountTest {
 
     @Ignore
     @Test
-    fun countMultipleOccurences() {
+    fun countMultipleOccurrences() {
         val expectedWordCount = mapOf("one" to 1, "fish" to 4, "two" to 1, "red" to 1, "blue" to 1)
 
         assertEquals(expectedWordCount, WordCount.phrase("one fish two fish red fish blue fish"))
@@ -30,16 +29,31 @@ class WordCountTest {
 
     @Ignore
     @Test
-    fun ignorePunctuation() {
-        val expectedWordCount = mapOf("car" to 1, "carpet" to 1, "as" to 1, "java" to 1, "javascript" to 1)
+    fun countCrampedList() {
+        val expectedWordCount = mapOf("one" to 1, "two" to 1, "three" to 1)
 
-        assertEquals(expectedWordCount, WordCount.phrase("car : carpet as java : javascript!!&@$%^&"))
-
+        assertEquals(expectedWordCount, WordCount.phrase("one,two,three"))
     }
 
     @Ignore
     @Test
-    fun includeNumbers() {
+    fun countExpandedList() {
+        val expectedWordCount = mapOf("one" to 1, "two" to 1, "three" to 1)
+
+        assertEquals(expectedWordCount, WordCount.phrase("one,\ntwo,\nthree"))
+    }
+
+    @Ignore
+    @Test
+    fun ignorePunctuation() {
+        val expectedWordCount = mapOf("car" to 1, "carpet" to 1, "as" to 1, "java" to 1, "javascript" to 1)
+
+        assertEquals(expectedWordCount, WordCount.phrase("car: carpet as java: javascript!!&@\$%^&"))
+    }
+
+    @Ignore
+    @Test
+    fun countsNumbers() {
         val expectedWordCount = mapOf("testing" to 2, "1" to 1, "2" to 1)
 
         assertEquals(expectedWordCount, WordCount.phrase("testing, 1, 2 testing"))
@@ -47,18 +61,26 @@ class WordCountTest {
 
     @Ignore
     @Test
-    fun normalizeCase() {
-        val expectedWordCount = mapOf("go" to 3)
+    fun countsCaseInsensitively() {
+        val expectedWordCount = mapOf("go" to 3, "stop" to 2)
 
-        assertEquals(expectedWordCount, WordCount.phrase("go Go GO"))
+        assertEquals(expectedWordCount, WordCount.phrase("go Go GO Stop stop"))
     }
 
     @Ignore
     @Test
-    fun allowApostrophes() {
+    fun allowsApostrophes() {
         val expectedWordCount = mapOf("first" to 1, "don't" to 2, "laugh" to 1, "then" to 1, "cry" to 1)
 
         assertEquals(expectedWordCount, WordCount.phrase("First: don't laugh. Then: don't cry."))
+    }
+
+    @Ignore
+    @Test
+    fun allowsQuotations() {
+        val expectedWordCount = mapOf("joe" to 1, "can't" to 1, "tell" to 1, "between" to 1, "large" to 2, "and" to 1)
+
+        assertEquals(expectedWordCount, WordCount.phrase("Joe can't tell between 'large' and large."))
     }
 
 }
