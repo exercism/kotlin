b4d8eafdc58f2b6e867b7aade9ea8ff973d25a1a
Stuart Kent <skent.dev@gmail.com>
2017-11-09 13:21:54 -0500

    pascals-triangle: update to v1.2.0
    https://github.com/exercism/problem-specifications/commit/2895b2b35af27be61196d8ec2b0c9c0fe120c197#diff-4355b236c6ba8355c589624ef3760d8b

--- exercises/pascals-triangle/src/test/kotlin/PascalsTriangleTest.kt
+++ exercises/pascals-triangle/src/test/kotlin/PascalsTriangleTest.kt
@@ -1,8 +1,14 @@
 import org.junit.Test
 import org.junit.Ignore
+import org.junit.Rule
+import org.junit.rules.ExpectedException
 import kotlin.test.assertEquals
 
 class PascalsTriangleTest {
+
+    @Rule
+    @JvmField
+    var expectedException: ExpectedException = ExpectedException.none()
 
     @Test
     fun zeroRows() {
@@ -58,8 +64,59 @@ class PascalsTriangleTest {
     }
 
     @Ignore
-    @Test(expected = IllegalArgumentException::class)
+    @Test
+    fun fiveRows() {
+        val expectedOutput = listOf(
+                listOf(1),
+                listOf(1, 1),
+                listOf(1, 2, 1),
+                listOf(1, 3, 3, 1),
+                listOf(1, 4, 6, 4, 1)
+        )
+
+        assertEquals(expectedOutput, PascalsTriangle.computeTriangle(5))
+    }
+
+    @Ignore
+    @Test
+    fun sixRows() {
+        val expectedOutput = listOf(
+                listOf(1),
+                listOf(1, 1),
+                listOf(1, 2, 1),
+                listOf(1, 3, 3, 1),
+                listOf(1, 4, 6, 4, 1),
+                listOf(1, 5, 10, 10, 5, 1)
+        )
+
+        assertEquals(expectedOutput, PascalsTriangle.computeTriangle(6))
+    }
+
+    @Ignore
+    @Test
+    fun tenRows() {
+        val expectedOutput = listOf(
+                listOf(1),
+                listOf(1, 1),
+                listOf(1, 2, 1),
+                listOf(1, 3, 3, 1),
+                listOf(1, 4, 6, 4, 1),
+                listOf(1, 5, 10, 10, 5, 1),
+                listOf(1, 6, 15, 20, 15, 6, 1),
+                listOf(1, 7, 21, 35, 35, 21, 7, 1),
+                listOf(1, 8, 28, 56, 70, 56, 28, 8, 1),
+                listOf(1, 9, 36, 84, 126, 126, 84, 36, 9, 1)
+        )
+
+        assertEquals(expectedOutput, PascalsTriangle.computeTriangle(10))
+    }
+
+    @Ignore
+    @Test
     fun validatesNotNegativeRows() {
+        expectedException.expect(IllegalArgumentException::class.java)
+        expectedException.expectMessage("Rows can't be negative!")
+
         PascalsTriangle.computeTriangle(-1)
     }
 
