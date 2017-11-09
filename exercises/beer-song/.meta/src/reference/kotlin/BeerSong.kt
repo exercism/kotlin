fc68219773a802fa9d726356744ed2549d0831c6
Stuart Kent <skent.dev@gmail.com>
2017-11-09 13:15:03 -0500

    beer-song: update to v2.0.0
    https://github.com/exercism/problem-specifications/commit/6b320c087cb1b9f6b94a2045ed80c0e60e304962#diff-390f6a03497503e5251431dce9c2345a

--- exercises/beer-song/.meta/src/reference/kotlin/BeerSong.kt
+++ exercises/beer-song/.meta/src/reference/kotlin/BeerSong.kt
@@ -1,7 +1,8 @@
 object BeerSong {
-    val lyrics: String by lazy { verses(99, 0) }
 
-    fun verse(number: Int) = when (number) {
+    fun verses(startBottles: Int, takeDown: Int) = (startBottles downTo takeDown).joinToString("\n") { verse(it) }
+
+    private fun verse(number: Int) = when (number) {
         0 -> "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
         1 -> "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
         2 -> "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
@@ -9,5 +10,4 @@ object BeerSong {
         else -> throw IllegalArgumentException("Invalid verse number: $number, must be within 0 and 99")
     }
 
-    fun verses(upperBound: Int, lowerBound: Int) = (upperBound downTo lowerBound).map { verse(it) }.joinToString("\n")
 }
