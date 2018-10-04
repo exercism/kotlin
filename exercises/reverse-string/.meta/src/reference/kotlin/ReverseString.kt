fa3774ee7e396f4ec77d0e284bcf036dc8452732
Valentin Michalak <valentinmichalak@gmail.com>
2018-10-04 11:56:04 +0200

    Update Reverse method

--- exercises/reverse-string/.meta/src/reference/kotlin/ReverseString.kt
+++ exercises/reverse-string/.meta/src/reference/kotlin/ReverseString.kt
@@ -1,1 +1,9 @@
-fun reverse(input: String): String = input.reversed()
+import java.lang.StringBuilder
+
+fun reverse(input: String): String {
+    var result = StringBuilder()
+    for(i in 0 until input.length) {
+        result.append(input[input.length - 1 - i])
+    }
+    return result.toString()
+}
