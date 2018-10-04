b71d8d9e1b71bbd6b62d6a3f2695d638b75e67b0
Christian Brunotte <cb@lathspell.de>
2018-10-05 00:04:48 +0200

    Added exercise diffie-hellman based on Java track (reference solution)

--- exercises/diffie-hellman/.meta/src/reference/kotlin/Squares.kt
+++
@@ -1,9 +0,0 @@
-class Squares(private val max: Int) {
-    private val range = 1..max
-
-    fun sumOfSquares() = range.map { it.squared() }.sum()
-    fun squareOfSum() = range.sum().squared()
-    fun difference() = squareOfSum() - sumOfSquares()
-
-    private fun Int.squared() = this * this
-}
