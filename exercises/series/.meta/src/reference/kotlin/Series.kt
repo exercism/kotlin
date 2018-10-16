ce3ea76aecc86f837f193e63ddb4dd769f6d81eb
Sophie Koonin <sophie.koonin@johnlewis.co.uk>
2018-10-16 17:54:53 +0100

    Add version for Series and update tests and implementation to match canonical data

--- exercises/series/.meta/src/reference/kotlin/Series.kt
+++ exercises/series/.meta/src/reference/kotlin/Series.kt
@@ -1,5 +1,13 @@
 object Series {
     fun Char.toDigitValue() = this.toInt() - '0'.toInt()
 
-    fun slices(n: Int, s: String): List<List<Int>> = s.dropLast(n - 1).mapIndexed { index, c -> s.subSequence(index, index + n).map { it.toDigitValue() } }
+    fun slices(n: Int, s: String): List<List<Int>> {
+        return when {
+            (n > s.length) -> throw IllegalArgumentException("slice length cannot be greater than series length")
+            (n == 0) -> throw IllegalArgumentException("slice length cannot be zero")
+            (n < 0) -> throw IllegalArgumentException("slice length cannot be negative")
+            (s == "") -> throw IllegalArgumentException("series cannot be empty")
+            else -> s.dropLast(n - 1).mapIndexed { index, c -> s.subSequence(index, index + n).map { it.toDigitValue() } }
+        }
+    }
 }
