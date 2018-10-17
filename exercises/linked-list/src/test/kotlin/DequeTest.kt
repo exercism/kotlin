9ce71a8e0b455d89ce2db08c835265adc87917b9
Mike Cifelli <mcifelli@aquent.com>
2018-10-17 10:13:01 -0400

    Add more linked-list tests

--- exercises/linked-list/src/test/kotlin/DequeTest.kt
+++ exercises/linked-list/src/test/kotlin/DequeTest.kt
@@ -11,13 +11,35 @@ class DequeTest {
         subject = Deque()
     }
 
+    @Test
+    fun emptyPop() {
+        assertEquals(null, subject.pop())
+    }
 
+    @Ignore
+    @Test
+    fun emptyShift() {
+        assertEquals(null, subject.shift())
+    }
+
+    @Ignore
     @Test
     fun pushPop() {
         subject.push(10)
         subject.push(20)
         assertEquals(20, subject.pop())
         assertEquals(10, subject.pop())
+        assertEquals(null, subject.pop())
+    }
+
+    @Ignore
+    @Test
+    fun pushPopEmptyShift() {
+        subject.push(10)
+        subject.push(20)
+        assertEquals(20, subject.pop())
+        assertEquals(10, subject.pop())
+        assertEquals(null, subject.shift())
     }
 
     @Ignore
@@ -27,6 +49,17 @@ class DequeTest {
         subject.push(20)
         assertEquals(10, subject.shift())
         assertEquals(20, subject.shift())
+        assertEquals(null, subject.shift())
+    }
+
+    @Ignore
+    @Test
+    fun pushShiftEmptyPop() {
+        subject.push(10)
+        subject.push(20)
+        assertEquals(10, subject.shift())
+        assertEquals(20, subject.shift())
+        assertEquals(null, subject.pop())
     }
 
     @Ignore
@@ -36,6 +69,17 @@ class DequeTest {
         subject.unshift(20)
         assertEquals(20, subject.shift())
         assertEquals(10, subject.shift())
+        assertEquals(null, subject.shift())
+    }
+
+    @Ignore
+    @Test
+    fun unshiftShiftEmptyPop() {
+        subject.unshift(10)
+        subject.unshift(20)
+        assertEquals(20, subject.shift())
+        assertEquals(10, subject.shift())
+        assertEquals(null, subject.pop())
     }
 
     @Ignore
@@ -45,6 +89,17 @@ class DequeTest {
         subject.unshift(20)
         assertEquals(10, subject.pop())
         assertEquals(20, subject.pop())
+        assertEquals(null, subject.pop())
+    }
+
+    @Ignore
+    @Test
+    fun unshiftPopEmptyShift() {
+        subject.unshift(10)
+        subject.unshift(20)
+        assertEquals(10, subject.pop())
+        assertEquals(20, subject.pop())
+        assertEquals(null, subject.shift())
     }
 
     @Ignore
@@ -62,5 +117,31 @@ class DequeTest {
         assertEquals(40, subject.shift())
         assertEquals(50, subject.pop())
         assertEquals(30, subject.shift())
+    }
+
+    @Ignore
+    @Test
+    fun shiftAndPop() {
+        subject.push(10)
+        subject.push(20)
+        subject.push(30)
+
+        assertEquals(10, subject.shift())
+        assertEquals(30, subject.pop())
+        assertEquals(20, subject.pop())
+        assertEquals(null, subject.pop())
+    }
+
+    @Ignore
+    @Test
+    fun popAndShift() {
+        subject.push(10)
+        subject.push(20)
+        subject.push(30)
+
+        assertEquals(30, subject.pop())
+        assertEquals(10, subject.shift())
+        assertEquals(20, subject.shift())
+        assertEquals(null, subject.shift())
     }
 }
