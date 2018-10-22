a7e6fd744ab454e138fe50bd3087d7d2e8e1c17f
William McGann <contact@WilliamMcGann.com>
2018-10-22 14:13:15 -0400

    Fix gradle-school exercise test

--- exercises/grade-school/src/test/kotlin/SchoolTest.kt
+++ exercises/grade-school/src/test/kotlin/SchoolTest.kt
@@ -75,7 +75,7 @@ class SchoolTest {
         school.add("Christopher", 4)
         school.add("Kyle", 3)
 
-        val expected = mapOf(6 to listOf("Kareem"), 4 to listOf("Christopher", "Jennifer"), 3 to listOf("Kyle"))
+        val expected = mapOf(3 to listOf("Kyle"), 4 to listOf("Jennifer", "Christopher"), 6 to listOf("Kareem"))
         val sortedClasses = school.sort()
         assertEquals(expected, sortedClasses)
         assertEquals(listOf(3, 4, 6), sortedClasses.keys.toList(), "Grades not sorted in ascending order")
