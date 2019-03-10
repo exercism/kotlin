c29b238c2a33cc8b23beadf25c119929532dba38
Mike Gehard <mike.gehard@gmail.com>
2019-03-10 10:57:27 -0600

    Make more idiomatic Kotlin without return

--- exercises/series/.meta/src/reference/kotlin/Series.kt
+++ exercises/series/.meta/src/reference/kotlin/Series.kt
@@ -1,13 +1,15 @@
 object Series {
     fun Char.toDigitValue() = this.toInt() - '0'.toInt()
 
-    fun slices(n: Int, s: String): List<List<Int>> {
-        return when {
+    fun slices(n: Int, s: String): List<List<Int>> =
+        when {
             (n > s.length) -> throw IllegalArgumentException("slice length cannot be greater than series length")
             (n == 0) -> throw IllegalArgumentException("slice length cannot be zero")
             (n < 0) -> throw IllegalArgumentException("slice length cannot be negative")
             (s == "") -> throw IllegalArgumentException("series cannot be empty")
-            else -> s.dropLast(n - 1).mapIndexed { index, c -> s.subSequence(index, index + n).map { it.toDigitValue() } }
+            else -> s.dropLast(n - 1)
+                .mapIndexed { index, c ->
+                    s.subSequence(index, index + n).map { it.toDigitValue() }
+                }
         }
-    }
 }
