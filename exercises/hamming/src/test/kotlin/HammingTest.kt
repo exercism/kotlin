484b21f34d18776047ca9e1f013f46636d4543fd
Uzi Landsmann <uzi.landsmann@bisnode.com>
2019-10-05 08:11:18 +0200

    Bumped hamming to 2.3.0 and changed the tests according to https://raw.githubusercontent.com/exercism/problem-specifications/master/exercises/hamming/canonical-data.json

--- exercises/hamming/src/test/kotlin/HammingTest.kt
+++ exercises/hamming/src/test/kotlin/HammingTest.kt
@@ -17,92 +17,43 @@ class HammingTest {
 
     @Ignore
     @Test
-    fun noDistanceBetweenShortIdenticalStrands() {
+    fun noDistanceBetweenSingleLetterIdenticalStrands() {
         assertEquals(0, Hamming.compute("A", "A"))
     }
 
     @Ignore
     @Test
+    fun completeDistanceInSingleLetterDifferentStrands() {
+        assertEquals(1, Hamming.compute("G", "T"))
+    }
+
+    @Ignore
+    @Test
     fun noDistanceBetweenLongIdenticalStrands() {
-        assertEquals(0, Hamming.compute("GGACTGA", "GGACTGA"))
+        assertEquals(0, Hamming.compute("GGACTGAAATCTG", "GGACTGAAATCTG"))
     }
 
     @Ignore
     @Test
-    fun completeDistanceInSingleNucleotideStrand() {
-        assertEquals(1, Hamming.compute("A", "G"))
-    }
-
-    @Ignore
-    @Test
-    fun completeDistanceInSmallStrand() {
-        assertEquals(2, Hamming.compute("AG", "CT"))
-    }
-
-    @Ignore
-    @Test
-    fun smallDistanceInSmallStrand() {
-        assertEquals(1, Hamming.compute("AT", "CT"))
-    }
-
-    @Ignore
-    @Test
-    fun smallDistanceInMediumStrand() {
-        assertEquals(1, Hamming.compute("GGACG", "GGTCG"))
-    }
-
-    @Ignore
-    @Test
-    fun smallDistanceInLongStrand() {
-        assertEquals(2, Hamming.compute("ACCAGGG", "ACTATGG"))
-    }
-
-    @Ignore
-    @Test
-    fun nonUniqueCharacterInFirstStrand() {
-        assertEquals(1, Hamming.compute("AAG", "AAA"))
-    }
-
-    @Ignore
-    @Test
-    fun nonUniqueCharacterInSecondStrand() {
-        assertEquals(1, Hamming.compute("AAA", "AAG"))
-    }
-
-    @Ignore
-    @Test
-    fun sameNucleotidesInDifferentPositions() {
-        assertEquals(2, Hamming.compute("TAG", "GAT"))
-    }
-
-    @Ignore
-    @Test
-    fun largeDistanceInPermutedStrand() {
-        assertEquals(4, Hamming.compute("GATACA", "GCATAA"))
-    }
-
-    @Ignore
-    @Test
-    fun largeDistanceInOffByOneStrand() {
+    fun largeDistanceBetweenLongDifferentStrands() {
         assertEquals(9, Hamming.compute("GGACGGATTCTG", "AGGACGGATTCT"))
     }
 
     @Ignore
     @Test
-    fun validatesFirstStrandNotLonger() {
+    fun disallowFirstStrandLonger() {
         expectedException.expect(IllegalArgumentException::class.java)
-        expectedException.expectMessage("left and right strands must be of equal length.")
+        expectedException.expectMessage("left and right strands must be of equal length")
 
         Hamming.compute("AATG", "AAA")
     }
 
     @Ignore
     @Test
-    fun validatesSecondStrandNotLonger() {
+    fun disallowSecondStrandLonger() {
         expectedException.expect(IllegalArgumentException::class.java)
-        expectedException.expectMessage("left and right strands must be of equal length.")
+        expectedException.expectMessage("left and right strands must be of equal length")
 
         Hamming.compute("ATA", "AGTG")
     }
-
 }
