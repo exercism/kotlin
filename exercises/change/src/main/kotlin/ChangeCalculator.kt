edef070ad988e463680afd1de6df6ede1af52b16
elena.parovyshnaya <elena.parovyshnaya@gmail.com>
2019-11-13 12:12:59 +0300

    #310: Create stub files for all exercises   - stubs for c-tasks: change, clock. collatz-conjecture   - redundant .keep-files are cleaned up

---
+++ exercises/change/src/main/kotlin/ChangeCalculator.kt
@@ -0,0 +1,7 @@
+class ChangeCalculator {
+    // TODO: implement proper constructor
+
+    fun computeMostEfficientChange(grandTotal: Int): List<Int> {
+        TODO("Implement the function to complete the task")
+    }
+}
