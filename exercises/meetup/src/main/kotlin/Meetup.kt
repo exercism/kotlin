248bc8fe9dfba5456288e40504ed883657b383c1
elena.parovyshnaya <elena.parovyshnaya@gmail.com>
2019-11-14 17:34:44 +0300

    #310: Create stub files for all exercises   - append stub impl for Meetup task

--- exercises/meetup/src/main/kotlin/Meetup.kt
+++ exercises/meetup/src/main/kotlin/Meetup.kt
@@ -1,5 +1,11 @@
-enum class MeetupSchedule {
+import java.time.DayOfWeek
+import java.time.LocalDate
 
-    FIRST, SECOND, THIRD, FOURTH, LAST, TEENTH
+class Meetup {
 
+    // TODO: implement proper constructor
+
+    fun day(dayOfWeek: DayOfWeek, schedule: MeetupSchedule): LocalDate{
+        TODO("Implement this function to complete the task")
+    }
 }
