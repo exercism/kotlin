5956dbcdb55bd588d94eb34d899f162b6f768b50
elena.parovyshnaya <elena.parovyshnaya@gmail.com>
2019-11-14 18:00:07 +0300

    #310: Create stub files for all exercises   - append stub impl for robot-simulator task

--- exercises/robot-simulator/src/main/kotlin/RobotSimulator.kt
+++ exercises/robot-simulator/src/main/kotlin/RobotSimulator.kt
@@ -1,1 +1,20 @@
-data class GridPosition(val x: Int, val y: Int)
+class RobotSimulator {
+
+    // TODO: implement proper constructor, provide read access to `gridPosition` and `orientation`
+
+    fun advance() {
+        TODO("Implement this function to complete the task")
+    }
+
+    fun turnLeft() {
+        TODO("Implement this function to complete the task")
+    }
+
+    fun turnRight() {
+        TODO("Implement this function to complete the task")
+    }
+
+    fun simulate(instructions: String) {
+        TODO("Implement this function to complete the task")
+    }
+}
