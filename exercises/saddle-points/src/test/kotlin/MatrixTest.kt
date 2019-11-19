6719db14001adb4a79c2703af4ded14c41dd7aff
elena.parovyshnaya <elena.parovyshnaya@gmail.com>
2019-11-19 13:40:36 +0300

    upgrade `saddle points` implementation to spec of 1.5.0 version: test suite refactoring  - empty coordinate sets are simplified

--- exercises/saddle-points/src/test/kotlin/MatrixTest.kt
+++ exercises/saddle-points/src/test/kotlin/MatrixTest.kt
@@ -25,7 +25,7 @@ class MatrixTest {
     fun `no saddle points for empty matrix`() =
         assertSaddlePointsEqual(
             Matrix(listOf(emptyList())),
-            emptySet<MatrixCoordinate>()
+            emptySet()
         )
 
     @Ignore
@@ -39,7 +39,7 @@ class MatrixTest {
                     listOf(2, 3, 1)
                 )
             ),
-            emptySet<MatrixCoordinate>()
+            emptySet()
         )
 
     @Ignore
