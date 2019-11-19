3378ac6ae2019d86d367a70cf7ca29ad8509f1d8
elena.parovyshnaya <elena.parovyshnaya@gmail.com>
2019-11-19 13:29:57 +0300

    upgrade `saddle points` implementation to spec of 1.5.0 version: test suite refactoring

--- exercises/saddle-points/src/test/kotlin/MatrixTest.kt
+++ exercises/saddle-points/src/test/kotlin/MatrixTest.kt
@@ -6,136 +6,128 @@ import kotlin.test.assertEquals
 class MatrixTest {
 
     @Test
-    fun `single saddle point`() {
-        val matrix = Matrix(
-            listOf(
-                listOf(9, 8, 7),
-                listOf(5, 3, 2),
-                listOf(6, 6, 7)
+    fun `single saddle point`() =
+        assertSaddlePointsEqual(
+            Matrix(
+                listOf(
+                    listOf(9, 8, 7),
+                    listOf(5, 3, 2),
+                    listOf(6, 6, 7)
+                )
+            ),
+            setOf(
+                MatrixCoordinate(2, 1)
             )
         )
 
-        val expectedSaddlePoints = setOf(MatrixCoordinate(2, 1))
-
-        assertEquals(expectedSaddlePoints, matrix.saddlePoints)
-    }
+    @Ignore
+    @Test
+    fun `no saddle points for empty matrix`() =
+        assertSaddlePointsEqual(
+            Matrix(listOf(emptyList())),
+            emptySet<MatrixCoordinate>()
+        )
 
     @Ignore
     @Test
-    fun `no saddle points for empty matrix`() {
-        val matrix = Matrix(listOf(emptyList()))
-
-        val expectedSaddlePoints = emptySet<MatrixCoordinate>()
-
-        assertEquals(expectedSaddlePoints, matrix.saddlePoints)
-    }
+    fun `no saddle points for nonempty matrix`() =
+        assertSaddlePointsEqual(
+            Matrix(
+                listOf(
+                    listOf(1, 2, 3),
+                    listOf(3, 1, 2),
+                    listOf(2, 3, 1)
+                )
+            ),
+            emptySet<MatrixCoordinate>()
+        )
 
     @Ignore
     @Test
-    fun `no saddle points for nonempty matrix`() {
-        val matrix = Matrix(
-            listOf(
-                listOf(1, 2, 3),
-                listOf(3, 1, 2),
-                listOf(2, 3, 1)
+    fun `multiple saddle points in a column`() =
+        assertSaddlePointsEqual(
+            Matrix(
+                listOf(
+                    listOf(4, 5, 4),
+                    listOf(3, 5, 5),
+                    listOf(1, 5, 4)
+                )
+            ),
+            setOf(
+                MatrixCoordinate(1, 2),
+                MatrixCoordinate(2, 2),
+                MatrixCoordinate(3, 2)
             )
         )
 
-        val expectedSaddlePoints = emptySet<MatrixCoordinate>()
-
-        assertEquals(expectedSaddlePoints, matrix.saddlePoints)
-    }
-
     @Ignore
     @Test
-    fun `multiple saddle points in a column`() {
-        val matrix = Matrix(
-            listOf(
-                listOf(4, 5, 4),
-                listOf(3, 5, 5),
-                listOf(1, 5, 4)
+    fun `multiple saddle points in a row`() =
+        assertSaddlePointsEqual(
+            Matrix(
+                listOf(
+                    listOf(6, 7, 8),
+                    listOf(5, 5, 5),
+                    listOf(7, 5, 6)
+                )
+            ),
+            setOf(
+                MatrixCoordinate(2, 1),
+                MatrixCoordinate(2, 2),
+                MatrixCoordinate(2, 3)
             )
         )
 
-        val expectedSaddlePoints = setOf(
-            MatrixCoordinate(1, 2),
-            MatrixCoordinate(2, 2),
-            MatrixCoordinate(3, 2)
-        )
-
-        assertEquals(expectedSaddlePoints, matrix.saddlePoints)
-    }
-
     @Ignore
     @Test
-    fun `multiple saddle points in a row`() {
-        val matrix = Matrix(
-            listOf(
-                listOf(6, 7, 8),
-                listOf(5, 5, 5),
-                listOf(7, 5, 6)
+    fun `saddle point in bottom right corner`() =
+        assertSaddlePointsEqual(
+            Matrix(
+                listOf(
+                    listOf(8, 7, 9),
+                    listOf(6, 7, 6),
+                    listOf(3, 2, 5)
+                )
+            ),
+            setOf(
+                MatrixCoordinate(3, 3)
             )
         )
 
-        val expectedSaddlePoints = setOf(
-            MatrixCoordinate(2, 1),
-            MatrixCoordinate(2, 2),
-            MatrixCoordinate(2, 3)
-        )
-
-        assertEquals(expectedSaddlePoints, matrix.saddlePoints)
-    }
-
     @Ignore
     @Test
-    fun `saddle point in bottom right corner`() {
-        val matrix = Matrix(
-            listOf(
-                listOf(8, 7, 9),
-                listOf(6, 7, 6),
-                listOf(3, 2, 5)
+    fun `saddle points in a single column matrix`() =
+        assertSaddlePointsEqual(
+            Matrix(
+                listOf(
+                    listOf(2),
+                    listOf(1),
+                    listOf(4),
+                    listOf(1)
+                )
+            ),
+            setOf(
+                MatrixCoordinate(2, 1),
+                MatrixCoordinate(4, 1)
             )
         )
 
-        val expectedSaddlePoints = setOf(MatrixCoordinate(3, 3))
-
-        assertEquals(expectedSaddlePoints, matrix.saddlePoints)
-    }
-
     @Ignore
     @Test
-    fun `saddle points in a single column matrix`() {
-        val matrix = Matrix(
-            listOf(
-                listOf(2),
-                listOf(1),
-                listOf(4),
-                listOf(1)
+    fun `saddle points in a single row matrix`() =
+        assertSaddlePointsEqual(
+            Matrix(
+                listOf(
+                    listOf(2, 5, 3, 5)
+                )
+            ),
+            setOf(
+                MatrixCoordinate(1, 2),
+                MatrixCoordinate(1, 4)
             )
         )
-
-        val expectedSaddlePoints = setOf(
-            MatrixCoordinate(2, 1),
-            MatrixCoordinate(4, 1)
-        )
-
-        assertEquals(expectedSaddlePoints, matrix.saddlePoints)
-    }
-
-    @Ignore
-    @Test
-    fun `saddle points in a single row matrix`() {
-        val matrix = Matrix(
-            listOf(
-                listOf(2, 5, 3, 5)
-            )
-        )
-
-        val expectedSaddlePoints = setOf(
-            MatrixCoordinate(1, 2),
-            MatrixCoordinate(1, 4)
-        )
-
-        assertEquals(expectedSaddlePoints, matrix.saddlePoints)
-    }
 }
+
+private fun assertSaddlePointsEqual(matrix: Matrix, coordinates: Set<MatrixCoordinate>) =
+    assertEquals(coordinates, matrix.saddlePoints)
+
