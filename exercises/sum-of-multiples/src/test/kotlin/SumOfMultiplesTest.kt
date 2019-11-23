97d26ff3c6f82c8a8a9b6d81afed0a70724c61ec
elena.parovyshnaya <elena.parovyshnaya@gmail.com>
2019-11-23 07:31:15 +0300

    upgrade `sum-of-multiples` test suite and ref impl according to spec of 1.5.0 version  - test set is replenished with new cases  - tests are renamed according to canonical case descriptions  - reference implementation is fixed to satisfy new tests set

--- exercises/sum-of-multiples/src/test/kotlin/SumOfMultiplesTest.kt
+++ exercises/sum-of-multiples/src/test/kotlin/SumOfMultiplesTest.kt
@@ -3,82 +3,82 @@ import org.junit.Test
 import kotlin.test.assertEquals
 
 class SumOfMultiplesTest {
-
     @Test
-    fun `multiples of 3 or 5 up to 1`() {
-        assertEquals(0, SumOfMultiples.sum(setOf(3, 5), 1))
-    }
+    fun `no multiples within limit`() =
+            assertEquals(0, SumOfMultiples.sum(setOf(3, 5), 1))
 
     @Test
     @Ignore
-    fun `multiples of 3 or 5 up to 4`() {
-        assertEquals(3, SumOfMultiples.sum(setOf(3, 5), 4))
-    }
+    fun `one factor has multiples within limit`() =
+            assertEquals(3, SumOfMultiples.sum(setOf(3, 5), 4))
 
     @Test
     @Ignore
-    fun `multiples of 3 up to 7`() {
-        assertEquals(9, SumOfMultiples.sum(setOf(3), 7))
-    }
+    fun `more than one multiple within limit`() =
+            assertEquals(9, SumOfMultiples.sum(setOf(3), 7))
 
     @Test
     @Ignore
-    fun `multiples of 3 or 5 up to 10`() {
-        assertEquals(23, SumOfMultiples.sum(setOf(3, 5), 10))
-    }
+    fun `more than one factor with multiples within limit`() =
+            assertEquals(23, SumOfMultiples.sum(setOf(3, 5), 10))
 
     @Test
     @Ignore
-    fun `multiples of 3 or 5 up to 100`() {
-        assertEquals(2318, SumOfMultiples.sum(setOf(3, 5), 100))
-    }
+    fun `each multiple is only counted once`() =
+            assertEquals(2318, SumOfMultiples.sum(setOf(3, 5), 100))
 
     @Test
     @Ignore
-    fun `multiples of 3 or 5 up to 1000`() {
-        assertEquals(233168, SumOfMultiples.sum(setOf(3, 5), 1000))
-    }
+    fun `much larger limit`() =
+            assertEquals(233168, SumOfMultiples.sum(setOf(3, 5), 1000))
 
     @Test
     @Ignore
-    fun `multiples of 7, 13 or 17 up to 20`() {
-        assertEquals(51, SumOfMultiples.sum(setOf(7, 13, 17), 20))
-    }
+    fun `three factors`() =
+            assertEquals(51, SumOfMultiples.sum(setOf(7, 13, 17), 20))
 
     @Test
     @Ignore
-    fun `multiples of 4 or 6 up to 15`() {
-        assertEquals(30, SumOfMultiples.sum(setOf(4, 6), 15))
-    }
+    fun `factors not relatively prime`() =
+            assertEquals(30, SumOfMultiples.sum(setOf(4, 6), 15))
 
     @Test
     @Ignore
-    fun `multiples of 5, 6 or 8 up to 150`() {
-        assertEquals(4419, SumOfMultiples.sum(setOf(5, 6, 8), 150))
-    }
+    fun `some pairs of factors relatively prime and some not`() =
+            assertEquals(4419, SumOfMultiples.sum(setOf(5, 6, 8), 150))
 
     @Test
     @Ignore
-    fun `multiples of 5 or 25 up to 51`() {
-        assertEquals(275, SumOfMultiples.sum(setOf(5, 25), 51))
-    }
+    fun `one factor is a multiple of another`() =
+            assertEquals(275, SumOfMultiples.sum(setOf(5, 25), 51))
 
     @Test
     @Ignore
-    fun `multiples of 43 or 47 up to 10000`() {
-        assertEquals(2203160, SumOfMultiples.sum(setOf(43, 47), 10000))
-    }
+    fun `much larger factors`() =
+            assertEquals(2203160, SumOfMultiples.sum(setOf(43, 47), 10000))
 
     @Test
     @Ignore
-    fun `multiples of 1 up to 100`() {
-        assertEquals(4950, SumOfMultiples.sum(setOf(1), 100))
-    }
+    fun `all numbers are multiples of 1`() =
+            assertEquals(4950, SumOfMultiples.sum(setOf(1), 100))
 
     @Test
     @Ignore
-    fun `multiples of an empty set up to 10000`() {
-        assertEquals(0, SumOfMultiples.sum(emptySet(), 10000))
-    }
+    fun `no factors means an empty sum`() =
+            assertEquals(0, SumOfMultiples.sum(emptySet(), 10000))
 
+    @Test
+    @Ignore
+    fun `the only multiple of 0 is 0`() =
+            assertEquals(0, SumOfMultiples.sum(setOf(1), 1))
+
+    @Test
+    @Ignore
+    fun `the factor 0 does not affect the sum of multiples of other factors`() =
+            assertEquals(3, SumOfMultiples.sum(setOf(3, 0), 4))
+
+    @Test
+    @Ignore
+    fun `include-exclude-based solutions must extend to cardinality greater than 3`() =
+            assertEquals(39614537, SumOfMultiples.sum(setOf(2, 3, 5, 7, 11), 10000))
 }
