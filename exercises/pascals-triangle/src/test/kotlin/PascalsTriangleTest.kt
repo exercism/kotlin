5dc4dbec08f0630cb4231866d98d3e3cd46a4d1b
elena.parovyshnaya <elena.parovyshnaya@gmail.com>
2019-12-05 11:32:15 +0300

    upgrade `pascals-triangle` test suite to spec of 1.5.0 version  - test set is diminished according to the specification: exceptional case processing is not required anymore, so here we also stop demanding it from a student  - tests are renamed with backticks

--- exercises/pascals-triangle/src/test/kotlin/PascalsTriangleTest.kt
+++ exercises/pascals-triangle/src/test/kotlin/PascalsTriangleTest.kt
@@ -1,123 +1,88 @@
 import org.junit.Test
 import org.junit.Ignore
-import org.junit.Rule
-import org.junit.rules.ExpectedException
 import kotlin.test.assertEquals
 
 class PascalsTriangleTest {
-
-    @Rule
-    @JvmField
-    var expectedException: ExpectedException = ExpectedException.none()
-
     @Test
-    fun zeroRows() {
-        val expectedOutput = emptyList<List<Int>>()
+    fun `zero rows`() = assertTriangleEquals(
+            0,
+            emptyList())
 
-        assertEquals(expectedOutput, PascalsTriangle.computeTriangle(0))
-    }
 
     @Ignore
     @Test
-    fun oneRow() {
-        val expectedOutput = listOf(
-                listOf(1)
-        )
-
-        assertEquals(expectedOutput, PascalsTriangle.computeTriangle(1))
-    }
+    fun `single row`() = assertTriangleEquals(
+            1,
+            listOf(
+                    listOf(1)))
 
     @Ignore
     @Test
-    fun twoRows() {
-        val expectedOutput = listOf(
-                listOf(1),
-                listOf(1, 1)
-        )
-
-        assertEquals(expectedOutput, PascalsTriangle.computeTriangle(2))
-    }
+    fun `two rows`() = assertTriangleEquals(
+            2,
+            listOf(
+                    listOf(1),
+                    listOf(1, 1)))
 
     @Ignore
     @Test
-    fun threeRows() {
-        val expectedOutput = listOf(
-                listOf(1),
-                listOf(1, 1),
-                listOf(1, 2, 1)
-        )
-
-        assertEquals(expectedOutput, PascalsTriangle.computeTriangle(3))
-    }
+    fun `tree rows`() = assertTriangleEquals(
+            3,
+            listOf(
+                    listOf(1),
+                    listOf(1, 1),
+                    listOf(1, 2, 1)))
 
     @Ignore
     @Test
-    fun fourRows() {
-        val expectedOutput = listOf(
-                listOf(1),
-                listOf(1, 1),
-                listOf(1, 2, 1),
-                listOf(1, 3, 3, 1)
-        )
-
-        assertEquals(expectedOutput, PascalsTriangle.computeTriangle(4))
-    }
+    fun `four rows`() = assertTriangleEquals(
+            4,
+            listOf(
+                    listOf(1),
+                    listOf(1, 1),
+                    listOf(1, 2, 1),
+                    listOf(1, 3, 3, 1)))
 
     @Ignore
     @Test
-    fun fiveRows() {
-        val expectedOutput = listOf(
-                listOf(1),
-                listOf(1, 1),
-                listOf(1, 2, 1),
-                listOf(1, 3, 3, 1),
-                listOf(1, 4, 6, 4, 1)
-        )
-
-        assertEquals(expectedOutput, PascalsTriangle.computeTriangle(5))
-    }
+    fun `five rows`() = assertTriangleEquals(
+            5,
+            listOf(
+                    listOf(1),
+                    listOf(1, 1),
+                    listOf(1, 2, 1),
+                    listOf(1, 3, 3, 1),
+                    listOf(1, 4, 6, 4, 1)))
 
     @Ignore
     @Test
-    fun sixRows() {
-        val expectedOutput = listOf(
-                listOf(1),
-                listOf(1, 1),
-                listOf(1, 2, 1),
-                listOf(1, 3, 3, 1),
-                listOf(1, 4, 6, 4, 1),
-                listOf(1, 5, 10, 10, 5, 1)
-        )
-
-        assertEquals(expectedOutput, PascalsTriangle.computeTriangle(6))
-    }
+    fun `six rows`() = assertTriangleEquals(
+            6,
+            listOf(
+                    listOf(1),
+                    listOf(1, 1),
+                    listOf(1, 2, 1),
+                    listOf(1, 3, 3, 1),
+                    listOf(1, 4, 6, 4, 1),
+                    listOf(1, 5, 10, 10, 5, 1)))
 
     @Ignore
     @Test
-    fun tenRows() {
-        val expectedOutput = listOf(
-                listOf(1),
-                listOf(1, 1),
-                listOf(1, 2, 1),
-                listOf(1, 3, 3, 1),
-                listOf(1, 4, 6, 4, 1),
-                listOf(1, 5, 10, 10, 5, 1),
-                listOf(1, 6, 15, 20, 15, 6, 1),
-                listOf(1, 7, 21, 35, 35, 21, 7, 1),
-                listOf(1, 8, 28, 56, 70, 56, 28, 8, 1),
-                listOf(1, 9, 36, 84, 126, 126, 84, 36, 9, 1)
-        )
-
-        assertEquals(expectedOutput, PascalsTriangle.computeTriangle(10))
-    }
-
-    @Ignore
-    @Test
-    fun validatesNotNegativeRows() {
-        expectedException.expect(IllegalArgumentException::class.java)
-        expectedException.expectMessage("Rows can't be negative!")
-
-        PascalsTriangle.computeTriangle(-1)
-    }
+    fun `ten rows`() = assertTriangleEquals(
+            10,
+            listOf(
+                    listOf(1),
+                    listOf(1, 1),
+                    listOf(1, 2, 1),
+                    listOf(1, 3, 3, 1),
+                    listOf(1, 4, 6, 4, 1),
+                    listOf(1, 5, 10, 10, 5, 1),
+                    listOf(1, 6, 15, 20, 15, 6, 1),
+                    listOf(1, 7, 21, 35, 35, 21, 7, 1),
+                    listOf(1, 8, 28, 56, 70, 56, 28, 8, 1),
+                    listOf(1, 9, 36, 84, 126, 126, 84, 36, 9, 1)))
 
 }
+
+private fun assertTriangleEquals(rows: Int, triangles: List<List<Int>>) =
+        assertEquals(triangles, PascalsTriangle.computeTriangle(rows))
