06923f211ba20967b818dcfac8b01308c8599e27
elena.parovyshnaya <elena.parovyshnaya@gmail.com>
2019-12-07 21:21:18 +0300

    upgrade `grade-school` exercise implementation to the specification of 1.0.0 version  -- reimplement test set basing on new dsl

--- exercises/grade-school/src/test/kotlin/SchoolTest.kt
+++ exercises/grade-school/src/test/kotlin/SchoolTest.kt
@@ -4,90 +4,86 @@ import kotlin.test.assertEquals
 
 class SchoolTest {
     @Test
-    fun `adding a student adds them to the sorted roster`() = assertRosterEquals(
-        mapOf(
-            "Aimee" to 2
-        ),
-        listOf("Aimee")
-    )
+    fun `adding a student adds them to the sorted roster`() =
+        students("Aimee" to 2)
+            .everyone()
+            .shouldBe(listOf("Aimee"))
 
     @Ignore
     @Test
     fun `adding more student adds them to the sorted roster`() =
-        assertRosterEquals(
-            mapOf(
-                "Blair" to 2,
-                "James" to 2,
-                "Paul" to 2
-            ),
-            listOf("Blair", "James", "Paul")
+        students(
+            "Blair" to 2,
+            "James" to 2,
+            "Paul" to 2
         )
+            .everyone()
+            .shouldBe(listOf("Blair", "James", "Paul"))
 
     @Ignore
     @Test
     fun `adding students to different grades adds them to the same sorted roster`() =
-        assertRosterEquals(
-            mapOf(
-                "Chelsea" to 3,
-                "Logan" to 7
-            ),
-            listOf("Chelsea", "Logan")
+        students(
+            "Chelsea" to 3,
+            "Logan" to 7
         )
+            .everyone()
+            .shouldBe(listOf("Chelsea", "Logan"))
 
     @Ignore
     @Test
     fun `roster returns an empty list if there are no students enrolled`() =
-        assertRosterEquals(
-            mapOf(),
-            listOf()
-        )
+        students()
+            .everyone()
+            .shouldBe(listOf())
 
     @Ignore
     @Test
     fun `student names with grades are displayed in the same sorted roster`() =
-        assertRosterEquals(
-            mapOf(
-                "Peter" to 2,
-                "Anna" to 1,
-                "Barb" to 1,
-                "Zoe" to 2,
-                "Alex" to 2,
-                "Jim" to 3,
-                "Charlie" to 1
-            ),
-            listOf("Anna", "Barb", "Charlie", "Alex", "Peter", "Zoe", "Jim")
+        students(
+            "Peter" to 2,
+            "Anna" to 1,
+            "Barb" to 1,
+            "Zoe" to 2,
+            "Alex" to 2,
+            "Jim" to 3,
+            "Charlie" to 1
         )
+            .everyone()
+            .shouldBe(listOf("Anna", "Barb", "Charlie", "Alex", "Peter", "Zoe", "Jim"))
 
     @Ignore
     @Test
     fun `grade returns the students in that grade in alphabetical order`() =
-        assertGradeEquals(
-            mapOf(
-                "Franklin" to 5,
-                "Bradley" to 5,
-                "Jeff" to 1
-            ),
-            5,
-            listOf("Bradley", "Franklin")
+        students(
+            "Franklin" to 5,
+            "Bradley" to 5,
+            "Jeff" to 1
         )
+            .fromGrade(5)
+            .shouldBe(listOf("Bradley", "Franklin"))
 
     @Ignore
     @Test
-    fun `grade returns an empty list if there are no students in that grade`() = assertGradeEquals(
-        mapOf(),
-        1,
-        listOf()
-    )
+    fun `grade returns an empty list if there are no students in that grade`() =
+        students()
+            .fromGrade(1)
+            .shouldBe(listOf())
 }
 
-private fun assertRosterEquals(students: Map<String, Int>, expectation: List<String>) =
-    with(School()) {
-        students.forEach { add(it.key, it.value) }
-        assertEquals(expectation, roster())
+private class Students(vararg students: Pair<String, Int>) {
+    private val school = School()
+
+    init {
+        students.forEach { school.add(it.first, it.second) }
     }
 
-private fun assertGradeEquals(students: Map<String, Int>, grade: Int, expectation: List<String>) =
-    with(School()) {
-        students.forEach { add(it.key, it.value) }
-        assertEquals(expectation, grade(grade))
-    }
+    fun fromGrade(grade: Int) = StudentNames(school.grade(grade))
+    fun everyone() = StudentNames(school.roster())
+}
+
+private class StudentNames(private val names: List<String>) {
+    fun shouldBe(names: List<String>) = assertEquals(names, this.names)
+}
+
+private fun students(vararg students: Pair<String, Int>) = Students(*students)
