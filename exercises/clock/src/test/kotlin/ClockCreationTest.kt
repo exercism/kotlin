580f63c96425b54938c2385e768472f510d1a80e
elena.parovyshnaya <elena.parovyshnaya@gmail.com>
2019-12-08 11:52:28 +0300

    upgrade `clock` exercise test set to the specification of 2.4.0 version  - test set is replenished and restructured according to the spec  - tests are renamed with backticks

--- exercises/clock/src/test/kotlin/ClockCreationTest.kt
+++ exercises/clock/src/test/kotlin/ClockCreationTest.kt
@@ -5,124 +5,83 @@ import kotlin.test.assertEquals
 class ClockCreationTest {
 
     @Test
-    fun canPrintTimeOnTheHour() {
-        assertEquals("08:00", Clock(8, 0).toString())
-    }
+    fun `on the hour`() = assertEquals("08:00", Clock(8, 0).toString())
 
     @Ignore
     @Test
-    fun canPrintTimeWithMinutes() {
-        assertEquals("11:09", Clock(11, 9).toString())
-    }
+    fun `past the hour`() = assertEquals("11:09", Clock(11, 9).toString())
 
     @Ignore
     @Test
-    fun midnightPrintsAsZero() {
-        assertEquals("00:00", Clock(24, 0).toString())
-    }
+    fun `midnight is zero hours`() = assertEquals("00:00", Clock(24, 0).toString())
 
     @Ignore
     @Test
-    fun hourRollsOver() {
-        assertEquals("01:00", Clock(25, 0).toString())
-    }
+    fun `hour rolls over`() = assertEquals("01:00", Clock(25, 0).toString())
 
     @Ignore
     @Test
-    fun hourRollsOverContinuously() {
-        assertEquals("04:00", Clock(100, 0).toString())
-    }
+    fun `hour rolls over continuously`() = assertEquals("04:00", Clock(100, 0).toString())
 
     @Ignore
     @Test
-    fun sixtyMinutesIsNextHour() {
-        assertEquals("02:00", Clock(1, 60).toString())
-    }
+    fun `sixty minutes is next hour`() = assertEquals("02:00", Clock(1, 60).toString())
 
     @Ignore
     @Test
-    fun minutesRollOver() {
-        assertEquals("02:40", Clock(0, 160).toString())
-    }
+    fun `minutes roll over`() = assertEquals("02:40", Clock(0, 160).toString())
 
     @Ignore
     @Test
-    fun minutesRollOverContinuously() {
-        assertEquals("04:43", Clock(0, 1723).toString())
-    }
+    fun `minutes roll over continuously`() = assertEquals("04:43", Clock(0, 1723).toString())
 
     @Ignore
     @Test
-    fun hourAndMinutesRollOver() {
-        assertEquals("03:40", Clock(25, 160).toString())
-    }
+    fun `hour and minutes roll over`() = assertEquals("03:40", Clock(25, 160).toString())
 
     @Ignore
     @Test
-    fun hourAndMinutesRollOverContinuously() {
-        assertEquals("11:01", Clock(201, 3001).toString())
-    }
+    fun `hour and minutes roll over continuously`() = assertEquals("11:01", Clock(201, 3001).toString())
 
     @Ignore
     @Test
-    fun hourAndMinutesRollOverToExactlyMidnight() {
-        assertEquals("00:00", Clock(72, 8640).toString())
-    }
+    fun `hour and minutes roll over to exactly midnight`() = assertEquals("00:00", Clock(72, 8640).toString())
 
     @Ignore
     @Test
-    fun negativeHour() {
-        assertEquals("23:15", Clock(-1, 15).toString())
-    }
+    fun `negative hour`() = assertEquals("23:15", Clock(-1, 15).toString())
 
     @Ignore
     @Test
-    fun negativeHourRollsOver() {
-        assertEquals("23:00", Clock(-25, 0).toString())
-    }
+    fun `negative hour rolls over`() = assertEquals("23:00", Clock(-25, 0).toString())
 
     @Ignore
     @Test
-    fun negativeHourRollsOverContinuously() {
-        assertEquals("05:00", Clock(-91, 0).toString())
-    }
+    fun `negative hour rolls over continuously`() = assertEquals("05:00", Clock(-91, 0).toString())
 
     @Ignore
     @Test
-    fun negativeMinutes() {
-        assertEquals("00:20", Clock(1, -40).toString())
-    }
+    fun `negative minutes`() = assertEquals("00:20", Clock(1, -40).toString())
 
     @Ignore
     @Test
-    fun negativeMinutesRollOver() {
-        assertEquals("22:20", Clock(1, -160).toString())
-    }
+    fun `negative minutes roll over`() = assertEquals("22:20", Clock(1, -160).toString())
 
     @Ignore
     @Test
-    fun negativeMinutesRollOverContinuously() {
-        assertEquals("16:40", Clock(1, -4820).toString())
-    }
+    fun `negative minutes roll over continuously`() = assertEquals("16:40", Clock(1, -4820).toString())
 
     @Ignore
     @Test
-    fun negativeHourAndMinutesBothRollOver() {
-        assertEquals("20:20", Clock(-25, -160).toString())
-    }
+    fun `negative sixty minutes is previous hour`() = assertEquals("01:00", Clock(2, -60).toString())
 
     @Ignore
     @Test
-    fun negativeHourAndMinutesBothRollOverContinuously() {
+    fun `negative hour and minutes both roll over`() = assertEquals("20:20", Clock(-25, -160).toString())
+
+    @Ignore
+    @Test
+    fun `negative hour and minutes both roll over continuously`() =
         assertEquals("22:10", Clock(-121, -5810).toString())
-    }
-
-    @Ignore
-    @Test
-    fun fullClockAndZeroedClock(){
-        val first = Clock(24,0)
-        val second = Clock(0,0)
-        assertEquals(first, second)
-    }
 
 }
