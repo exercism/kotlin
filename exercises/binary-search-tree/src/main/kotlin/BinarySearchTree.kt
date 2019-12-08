34711f3303e3a78dd192646ea94d48f727601e83
Christian Brunotte <cb@lathspell.de>
2019-12-08 15:10:01 +0100

    Added new exercise 'binary-search-tree'

---
+++ exercises/binary-search-tree/src/main/kotlin/BinarySearchTree.kt
@@ -0,0 +1,19 @@
+class BinarySearchTree<T : Comparable<T>> {
+
+    data class Node<T>(val data: T /* maybe more... */)
+
+    var root: Node<T>? = null
+
+    fun insert(value: T) {
+        TODO("Delete this statement and write your own implementation.")
+    }
+
+    fun asSortedList(): List<T> {
+        TODO("Delete this statement and write your own implementation.")
+    }
+
+    fun asLevelOrderList(): List<T> {
+        TODO("Delete this statement and write your own implementation.")
+    }
+
+}
---
+++ exercises/binary-search-tree/src/test/kotlin/BinarySearchTreeTest.kt
@@ -0,0 +1,127 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+
+class BinarySearchTreeTest {
+
+    @Test
+    fun `data is retained`() {
+        val tree = BinarySearchTree<Int>()
+        val expected = 4
+        tree.insert(expected)
+        val root = tree.root
+        assertNotNull(root)
+        val actual = root.data
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `inserts less`() {
+        val tree = BinarySearchTree<Char>()
+        val expectedRoot = '4'
+        val expectedLeft = '2'
+        tree.insert(expectedRoot)
+        tree.insert(expectedLeft)
+        val root = tree.root
+        assertNotNull(root)
+        val left = root.left
+        assertNotNull(left)
+        val actualRoot = root.data
+        val actualLeft = left.data
+        assertEquals(expectedLeft, actualLeft)
+        assertEquals(expectedRoot, actualRoot)
+    }
+
+    @Test
+    fun `inserts same`() {
+        val tree = BinarySearchTree<String>()
+        val expectedRoot = "4"
+        val expectedLeft = "4"
+        tree.insert(expectedRoot)
+        tree.insert(expectedLeft)
+        val root = tree.root
+        assertNotNull(root)
+        val left = root.left
+        assertNotNull(left)
+        val actualRoot = root.data
+        val actualLeft = left.data
+        assertEquals(expectedLeft, actualLeft)
+        assertEquals(expectedRoot, actualRoot)
+    }
+
+    @Test
+    fun `inserts right`() {
+        val tree = BinarySearchTree<Int>()
+        val expectedRoot = 4
+        val expectedRight = 5
+        tree.insert(expectedRoot)
+        tree.insert(expectedRight)
+        val root = tree.root
+        assertNotNull(root)
+        val right = root.right
+        assertNotNull(right)
+        val actualRoot = root.data
+        val actualRight = right.data
+        assertEquals(expectedRight, actualRight)
+        assertEquals(expectedRoot, actualRoot)
+    }
+
+    @Test
+    fun `creates complex tree`() {
+        val tree = BinarySearchTree<Char>()
+        val expected = listOf('4', '2', '6', '1', '3', '5', '7')
+        val treeData = listOf('4', '2', '6', '1', '3', '5', '7')
+        treeData.forEach(tree::insert)
+        val actual = tree.asLevelOrderList()
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `sorts single element`() {
+        val tree = BinarySearchTree<String>()
+        val expected = listOf("2")
+        tree.insert("2")
+        val actual = tree.asSortedList()
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `sorts collection of two if second inserted is smaller than first`() {
+        val tree = BinarySearchTree<Int>()
+        val expected: List<Int> = listOf(1, 2)
+        tree.insert(2)
+        tree.insert(1)
+        val actual = tree.asSortedList()
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `sorts collection of two if second number is same as first`() {
+        val tree = BinarySearchTree<Char>()
+        val expected = listOf('2', '2')
+        tree.insert('2')
+        tree.insert('2')
+        val actual = tree.asSortedList()
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `sorts collection of two if second inserted is bigger than first`() {
+        val tree = BinarySearchTree<Char>()
+        val expected = listOf('2', '3')
+        tree.insert('2')
+        tree.insert('3')
+        val actual = tree.asSortedList()
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `iterates over complex tree`() {
+        val tree = BinarySearchTree<String>()
+        val expected = listOf("1", "2", "3", "5", "6", "7")
+        val treeData = listOf("2", "1", "3", "6", "7", "5")
+        treeData.forEach(tree::insert)
+        val actual = tree.asSortedList()
+        assertEquals(expected, actual)
+    }
+}
