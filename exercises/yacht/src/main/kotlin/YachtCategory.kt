8f5f631d185292baa9a38a9bcfbc880172b27b66
Christian Brunotte <cb@lathspell.de>
2019-12-19 23:41:08 +0100

    Added new exercise 'yacht'

---
+++ exercises/yacht/src/main/kotlin/YachtCategory.kt
@@ -0,0 +1,14 @@
+enum class YachtCategory {
+    YACHT,
+    ONES,
+    TWOS,
+    THREES,
+    FOURS,
+    FIVES,
+    SIXES,
+    FULL_HOUSE,
+    FOUR_OF_A_KIND,
+    LITTLE_STRAIGHT,
+    BIG_STRAIGHT,
+    CHOICE
+}
