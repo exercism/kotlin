256d00adad961bb8bf6f14ffa97940e237685d16
Christian Brunotte <cb@lathspell.de>
2020-01-04 21:56:37 +0100

    Added new exercise 'resistor-color-trio'

---
+++ exercises/resistor-color-trio/.meta/src/reference/kotlin/ResistorColorTrio.kt
@@ -0,0 +1,17 @@
+import kotlin.math.log10
+import kotlin.math.pow
+
+object ResistorColorTrio {
+
+    fun text(vararg input: Color): String {
+        val duoValue = 10 * input[0].ordinal + input[1].ordinal
+        val trioValue = duoValue * 10.0.pow(input[2].ordinal)
+
+        val thousandPowers = (log10(trioValue) / 3).toInt()
+        val resultValue = (trioValue / 1000.0.pow(thousandPowers)).toInt()
+        val resultUnit = Unit.values()[thousandPowers].name.toLowerCase()
+
+        return "$resultValue $resultUnit"
+    }
+
+}
