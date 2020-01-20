b19b503f1e9c5f88166e392bd9d3a7f431e2b1aa
Christian Brunotte <cb@lathspell.de>
2020-01-20 01:20:17 +0100

    Added new exercise 'run-length-encoding'

---
+++ exercises/run-length-encoding/src/main/kotlin/RunLengthEncoding.kt
@@ -0,0 +1,10 @@
+object RunLengthEncoding {
+
+    fun encode(input: String): String {
+        TODO("Implement this to complete the task")
+    }
+
+    fun decode(input: String): String {
+        TODO("Implement this to complete the task")
+    }
+}
