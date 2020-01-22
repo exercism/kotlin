79723da2f210eaebb4c26385279b64ddc4269903
Christian Brunotte <cb@lathspell.de>
2020-01-22 10:14:25 +0100

    Added new exercise 'transpose' (#413)

---
+++ exercises/transpose/.meta/src/reference/kotlin/Transpose.kt
@@ -0,0 +1,13 @@
+object Transpose {
+
+    fun transpose(input: List<String>): List<String> =
+            (0 until (input.map { it.length }.max() ?: 0)).map { x: Int ->
+                input.indices.joinToString("") { y: Int ->
+                    if (x < input[y].length) {
+                        input[y][x].toString()
+                    } else {
+                        "°"
+                    }
+                }.trimEnd('°').replace('°', ' ')
+            }
+}
---
+++ exercises/transpose/src/main/kotlin/Transpose.kt
@@ -0,0 +1,6 @@
+object Transpose {
+
+    fun transpose(input: List<String>): List<String> {
+        TODO("Implement this to complete the task")
+    }
+}
