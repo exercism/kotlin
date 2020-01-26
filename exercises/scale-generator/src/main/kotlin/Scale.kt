b5bce0c1a5793d7d9e1e1282f3f4f5968ee25f17
Christian Brunotte <cb@lathspell.de>
2020-01-26 13:48:12 +0100

    Added new exercise 'scale-generator'

---
+++ exercises/scale-generator/src/main/kotlin/Scale.kt
@@ -0,0 +1,11 @@
+class Scale(private val tonic: String) {
+
+    fun chromatic(): List<String> {
+        TODO("Implement this function to complete the task")
+    }
+
+    fun interval(intervals: String): List<String> {
+        TODO("Implement this function to complete the task")
+    }
+
+}
