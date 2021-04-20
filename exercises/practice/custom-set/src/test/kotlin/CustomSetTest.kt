0b3104ff5f3ec732865bbf38fafe773b5a4a3be6
J.Tadebois <jpm.tadebois@gmail.com>
2021-04-20 23:26:23 +0400

    Fix erroneous unit test expected value

--- exercises/practice/custom-set/src/test/kotlin/CustomSetTest.kt
+++ exercises/practice/custom-set/src/test/kotlin/CustomSetTest.kt
@@ -280,7 +280,7 @@ class CustomSetTest {
     fun `union of non empty sets contains all unique elements`() {
         val set1 = CustomSet(1, 3)
         val set2 = CustomSet(2, 3)
-        val expected = CustomSet(3, 2, 1)
+        val expected = CustomSet(1, 2, 3)
         assertEquals(expected, set1 + set2)
     }
 }
