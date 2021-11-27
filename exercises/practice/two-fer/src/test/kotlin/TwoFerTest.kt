96c78e6e4852d848538473c0fa038765a47f5cef
David Gaspar <37712275+davidgaspardev@users.noreply.github.com>
2021-11-27 02:06:12 -0300

    Update TwoFerTest.kt
    Following the exercise description to provide a complete test.

--- exercises/practice/two-fer/src/test/kotlin/TwoFerTest.kt
+++ exercises/practice/two-fer/src/test/kotlin/TwoFerTest.kt
@@ -10,21 +10,18 @@ class TwoFerTest {
     }
 
     @Test
-    @Ignore
     fun aNameGiven() {
         assertEquals("One for Alice, one for me.", twofer("Alice"))
     }
 
     @Test
-    @Ignore
     fun anotherNameGiven() {
         assertEquals("One for Bob, one for me.", twofer("Bob"))
     }
 
     @Test
-    @Ignore
     fun emptyStringGiven() {
-        assertEquals("One for , one for me.", twofer(""))
+        assertEquals("One for you, one for me.", twofer(""))
     }
 
 }
