e85ebaa35965e3db69137c59b0dfec3be2a69eae
Emilio Lopez Gabeiras <emiliolg@mac.com>
2022-03-20 19:08:55 -0300

    Add Allergen.kt to .meta so local tests run

---
+++ exercises/practice/allergies/.meta/src/reference/kotlin/Allergen.kt
@@ -0,0 +1,10 @@
+enum class Allergen(val score: Int) {
+    EGGS(1),
+    PEANUTS(2),
+    SHELLFISH(4),
+    STRAWBERRIES(8),
+    TOMATOES(16),
+    CHOCOLATE(32),
+    POLLEN(64),
+    CATS(128)
+}
