b955d48924f3a70ebe5513702cd3413dd9c2bb41
Simeon L <l.simeon@teamhelios.dev>
2024-10-08 11:05:47 +0200

    fix: removed the @Ignore from the Tests

--- exercises/practice/acronym/src/test/kotlin/AcronymTest.kt
+++ exercises/practice/acronym/src/test/kotlin/AcronymTest.kt
@@ -8,49 +8,41 @@ class AcronymTest {
             "Portable Network Graphics",
             "PNG")
 
-    @Ignore
     @Test
     fun `lowercase word in the middle`() = assertAcronymEquals(
             "Ruby on Rails",
             "ROR")
 
-    @Ignore
     @Test
     fun `punctuation is ignored`() = assertAcronymEquals(
             "First In, First Out",
             "FIFO")
 
-    @Ignore
     @Test
     fun `uppercased word at the start`() = assertAcronymEquals(
             "GNU Image Manipulation Program",
             "GIMP")
 
-    @Ignore
     @Test
     fun `hyphen is ignored`() = assertAcronymEquals(
             "Complementary metal-oxide semiconductor",
             "CMOS")
 
-    @Ignore
     @Test
     fun `lots of words`() = assertAcronymEquals(
             "Rolling On The Floor Laughing So Hard That My Dogs Came Over And Licked Me",
             "ROTFLSHTMDCOALM")
 
-    @Ignore
     @Test
     fun `consecutive delimiters`() = assertAcronymEquals(
             "Something - I made up from thin air",
             "SIMUFTA")
 
-    @Ignore
     @Test
     fun `apostrophe is ignored`() = assertAcronymEquals(
             "Halley's Comet",
             "HC")
 
-    @Ignore
     @Test
     fun `underscore emphasis is ignored`() = assertAcronymEquals(
             "The Road _Not_ Taken",
