776468c1353952c00e42b23cd8e65e28329c4586
Simeon L. <hi@simeon.lol>
2024-10-14 13:26:31 +0200

    fix: Added @Ignore to the tests

--- exercises/practice/resistor-color-duo/src/test/kotlin/ResistorColorDuoTest.kt
+++ exercises/practice/resistor-color-duo/src/test/kotlin/ResistorColorDuoTest.kt
@@ -1,3 +1,4 @@
+import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -6,15 +7,19 @@ class ResistorColorDuoTest {
     @Test
     fun `brown and black`() = assertEquals(10, ResistorColorDuo.value(Color.BROWN, Color.BLACK))
 
+    @Ignore
     @Test
     fun `blue and grey`() = assertEquals(68, ResistorColorDuo.value(Color.BLUE, Color.GREY))
 
+    @Ignore
     @Test
     fun `yellow and violet`() = assertEquals(47, ResistorColorDuo.value(Color.YELLOW, Color.VIOLET))
 
+    @Ignore
     @Test
     fun `orange and orange`() = assertEquals(33, ResistorColorDuo.value(Color.ORANGE, Color.ORANGE))
 
+    @Ignore
     @Test
     fun `ignore additional colors`() = assertEquals(51, ResistorColorDuo.value(Color.GREEN, Color.BROWN, Color.ORANGE))
 
--- exercises/practice/resistor-color/src/test/kotlin/ResistorColorTest.kt
+++ exercises/practice/resistor-color/src/test/kotlin/ResistorColorTest.kt
@@ -1,3 +1,4 @@
+import kotlin.test.Ignore
 import kotlin.test.assertEquals
 import kotlin.test.Test
 
@@ -6,12 +7,15 @@ class ResistorColorTest {
     @Test
     fun testBlackColorCode() = assertEquals(0, ResistorColor.colorCode("black"))
 
+    @Ignore
     @Test
     fun testWhiteColorCode() = assertEquals(9, ResistorColor.colorCode("white"))
 
+    @Ignore
     @Test
     fun testOrangeColorCode() = assertEquals(3, ResistorColor.colorCode("orange"))
 
+    @Ignore
     @Test
     fun testColors() {
         val expected = listOf("black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white")
