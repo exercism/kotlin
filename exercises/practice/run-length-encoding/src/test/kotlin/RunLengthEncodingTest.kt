3b4bc87e26ae18134101cf3f93bbda4413d0766d
Simeon L. <78266888+DerSimeon@users.noreply.github.com>
2024-10-20 02:57:11 +0200

    fix: Added @Ignore to the tests (#684)
    [no important files changed]

--- exercises/practice/run-length-encoding/src/test/kotlin/RunLengthEncodingTest.kt
+++ exercises/practice/run-length-encoding/src/test/kotlin/RunLengthEncodingTest.kt
@@ -1,49 +1,61 @@
 import kotlin.test.Test
 import kotlin.test.assertEquals
-
 import RunLengthEncoding.encode
 import RunLengthEncoding.decode
+import kotlin.test.Ignore
 
 class RunLengthEncodingTest {
 
     @Test
     fun `encode_empty string`() = assertEquals("", encode(""))
 
+    @Ignore
     @Test
     fun `encode_single characters only are encoded without count`() = assertEquals("XYZ", encode("XYZ"))
 
+    @Ignore
     @Test
     fun `encode_string with no single characters`() = assertEquals("2A3B4C", encode("AABBBCCCC"))
 
+    @Ignore
     @Test
     fun `encode_single characters mixed with repeated characters`() =
             assertEquals("12WB12W3B24WB", encode("WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB"))
 
+    @Ignore
     @Test
     fun `encode_multiple whitespace mixed in string`() = assertEquals("2 hs2q q2w2 ", encode("  hsqq qww  "))
 
+    @Ignore
     @Test
     fun `encode_lowercase characters`() = assertEquals("2a3b4c", encode("aabbbcccc"))
 
+    @Ignore
     @Test
     fun `decode_empty string`() = assertEquals("", decode(""))
 
+    @Ignore
     @Test
     fun `decode_single characters only`() = assertEquals("XYZ", decode("XYZ"))
 
+    @Ignore
     @Test
     fun `decode_string with no single characters`() = assertEquals("AABBBCCCC", decode("2A3B4C"))
 
+    @Ignore
     @Test
     fun `decode_single characters with repeated characters`() =
             assertEquals("WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB", decode("12WB12W3B24WB"))
 
+    @Ignore
     @Test
     fun `decode_multiple whitespace mixed in string`() = assertEquals("  hsqq qww  ", decode("2 hs2q q2w2 "))
 
+    @Ignore
     @Test
     fun `decode_lower case string`() = assertEquals("aabbbcccc", decode("2a3b4c"))
 
+    @Ignore
     @Test
     fun `encode followed by decode gives original string`() = assertEquals("zzz ZZ  zZ", decode(encode("zzz ZZ  zZ")))
 
