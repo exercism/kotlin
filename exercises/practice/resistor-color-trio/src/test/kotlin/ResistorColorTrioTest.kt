a64fcb9fe48df9864a20639bf86f788e5f608b7d
Simeon L. <78266888+DerSimeon@users.noreply.github.com>
2024-10-20 02:57:01 +0200

    fix: Added @Ignore to the tests (#683)
    [no important files changed]

--- exercises/practice/resistor-color-trio/src/test/kotlin/ResistorColorTrioTest.kt
+++ exercises/practice/resistor-color-trio/src/test/kotlin/ResistorColorTrioTest.kt
@@ -1,24 +1,30 @@
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import Color.*
+import kotlin.test.Ignore
 
 class ResistorColorTrioTest {
 
     @Test
     fun `orange orange black`() = assertEquals("33 ohms", ResistorColorTrio.text(ORANGE, ORANGE, BLACK))
 
+    @Ignore
     @Test
     fun `blue grey brown`() = assertEquals("680 ohms", ResistorColorTrio.text(BLUE, GREY, BROWN))
 
+    @Ignore
     @Test
     fun `red black red`() = assertEquals("2 kiloohms", ResistorColorTrio.text(RED, BLACK, RED))
 
+    @Ignore
     @Test
     fun `green brown orange`() = assertEquals("51 kiloohms", ResistorColorTrio.text(GREEN, BROWN, ORANGE))
 
+    @Ignore
     @Test
     fun `yellow violet yellow`() = assertEquals("470 kiloohms", ResistorColorTrio.text(YELLOW, VIOLET, YELLOW))
 
+    @Ignore
     @Test
     fun `yellow violet violet`() = assertEquals("470 megaohms", ResistorColorTrio.text(YELLOW, VIOLET, VIOLET))
 }
