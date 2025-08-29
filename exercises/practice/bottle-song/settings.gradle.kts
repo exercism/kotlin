aa1d04bc06793e113e900d694babb8c59a065677
Kah Goh <villastar@yahoo.com.au>
2025-08-30 05:23:54 +0800

    Replace beer song with bottle song (#705)
    * Replace beer song with bottle song
    
    * Make bottle song difficulty same as beer song

---
+++ exercises/practice/bottle-song/settings.gradle.kts
@@ -0,0 +1,13 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.id) {
+                "org.jetbrains.kotlin.jvm" -> useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
+            }
+        }
+    }
+}
