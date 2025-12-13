4b41ab1697b30b7b48f9b00052da59bc62fda780
Yao Dossavi <nouketudiant228@gmail.com>
2025-12-14 00:05:56 +0100

    Update Bob.kt

--- exercises/practice/bob/src/main/kotlin/Bob.kt
+++ exercises/practice/bob/src/main/kotlin/Bob.kt
@@ -1,5 +1,26 @@
 object Bob {
     fun hey(input: String): String {
-        TODO("Implement the function to complete the task")
+        //TODO("Implement the function to complete the task")
+    
+        var response = ""
+    
+        if (input.last() == '?'){
+            response = "Sure."
+        }
+        else {
+            for (elt in input){
+                if (elt.isUpperCase()) {
+                    response = "Whoa, chill out!"
+                } else if(elt.isUpperCase() && input.last() == '?'){
+                    response = "Calm down, I know what I'm doing!"
+                } else if (input.isEmpty() || input.isBlank()) {
+                    response = "Fine. Be that way!"
+                } else {
+                    response = "Whatever"
+                }
+            }
+    
+        }
+        return response
     }
 }
