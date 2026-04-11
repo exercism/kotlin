259b8957dd189a50d7faf66b71a7c7abbbebd3cc
Andrew Garvin <garvin.drew@gmail.com>
2026-04-11 11:04:07 -0400

    Test names use backticks and spaces

--- exercises/practice/change/src/test/kotlin/ChangeCalculatorTest.kt
+++ exercises/practice/change/src/test/kotlin/ChangeCalculatorTest.kt
@@ -18,35 +18,35 @@ class ChangeCalculatorTest {
 
     @Ignore
     @Test
-    fun singleCoinChange() {
+    fun `single coin change`() {
         val computedChange = ChangeCalculator(listOf(1, 5, 10, 25, 100)).computeMostEfficientChange(25)
         assertContainsExactly(computedChange, listOf(25))
     }
 
     @Ignore
     @Test
-    fun multipleCoinChange() {
+    fun `multiple coin change`() {
         val computedChange = ChangeCalculator(listOf(1, 5, 10, 25, 100)).computeMostEfficientChange(15)
         assertContainsExactly(computedChange, listOf(5, 10))
     }
 
     @Ignore
     @Test
-    fun changeWithLilliputianCoins() {
+    fun `change with Lilliputian Coins`() {
         val computedChange = ChangeCalculator(listOf(1, 4, 15, 20, 50)).computeMostEfficientChange(23)
         assertContainsExactly(computedChange, listOf(4, 4, 15))
     }
 
     @Ignore
     @Test
-    fun changeWithLowerElboniaCoins() {
+    fun `change with Lower Elbonia Coins`() {
         val computedChange = ChangeCalculator(listOf(1, 5, 10, 21, 25)).computeMostEfficientChange(63)
         assertContainsExactly(computedChange, listOf(21, 21, 21))
     }
 
     @Ignore
     @Test
-    fun largeTargetValues() {
+    fun `large target values`() {
         val computedChange = ChangeCalculator(listOf(1, 2, 5, 10, 20, 50, 100)).computeMostEfficientChange(999)
         assertContainsExactly(
             computedChange,
@@ -56,14 +56,14 @@ class ChangeCalculatorTest {
 
     @Ignore
     @Test
-    fun possibleChangeWithoutUnitCoinsAvailable() {
+    fun `possible change without unit coins available`() {
         val computedChange = ChangeCalculator(listOf(2, 5, 10, 20, 50)).computeMostEfficientChange(21)
         assertContainsExactly(computedChange, listOf(2, 2, 2, 5, 10))
     }
 
     @Ignore
     @Test
-    fun anotherPossibleChangeWithoutUnitCoinsAvailable() {
+    fun `another possible change without unit coins available`() {
         val computedChange = ChangeCalculator(listOf(4, 5)).computeMostEfficientChange(27)
         assertContainsExactly(computedChange, listOf(4, 4, 4, 5, 5, 5))
     }
@@ -77,14 +77,14 @@ class ChangeCalculatorTest {
 
     @Ignore
     @Test
-    fun noCoinsMake0Change() {
+    fun `no coins make 0 change`() {
         val computedChange = ChangeCalculator(listOf(1, 5, 10, 21, 25)).computeMostEfficientChange(0)
         assertEquals(0, computedChange.size)
     }
 
     @Ignore
     @Test
-    fun errorTestingForChangeSmallerThanTheSmallestCoin() {
+    fun `error testing for change smaller than the smallest of coins`() {
         val changeCalculator = ChangeCalculator(listOf(5, 10))
 
         expectedException.expect(IllegalArgumentException::class.java)
@@ -95,7 +95,7 @@ class ChangeCalculatorTest {
 
     @Ignore
     @Test
-    fun errorIfNoCombinationCanAddUpToTarget() {
+    fun `error if no combination can add up to target`() {
         val changeCalculator = ChangeCalculator(listOf(5, 10))
 
         expectedException.expect(IllegalArgumentException::class.java)
@@ -106,7 +106,7 @@ class ChangeCalculatorTest {
 
     @Ignore
     @Test
-    fun cannotFindNegativeChangeValues() {
+    fun `cannot find negative change values`() {
         val changeCalculator = ChangeCalculator(listOf(1, 2, 5))
 
         expectedException.expect(IllegalArgumentException::class.java)
