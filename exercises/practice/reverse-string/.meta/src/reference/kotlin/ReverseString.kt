a6e4661d0111a593d747dfc351b626f645376984
Andrew Garvin <garvin.drew@gmail.com>
2026-08-31 13:28:33 -0400

    updated reference solution to accomodate new grapheme tests

--- exercises/practice/reverse-string/.meta/src/reference/kotlin/ReverseString.kt
+++ exercises/practice/reverse-string/.meta/src/reference/kotlin/ReverseString.kt
@@ -1,7 +1,18 @@
+import java.text.BreakIterator
+
 fun reverse(input: String): String {
-    val result = StringBuilder()
-    for(i in input.indices) {
-        result.append(input[input.length - 1 - i])
+    val iterator = BreakIterator.getCharacterInstance()
+    iterator.setText(input)
+
+    val graphemes = mutableListOf<String>()
+    var start = iterator.first()
+    var end = iterator.next()
+
+    while (end != BreakIterator.DONE) {
+        graphemes.add(input.substring(start, end))
+        start = end
+        end = iterator.next()
     }
-    return result.toString()
+
+    return graphemes.asReversed().joinToString("")
 }
